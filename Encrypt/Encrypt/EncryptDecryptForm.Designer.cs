91626edd887080f7100c3c2cd1b3d8afe5b655e6
ram.kumar <ram.kumar@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-01-28 07:15:28 +0000

    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@19 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ Encrypt/Encrypt/EncryptDecryptForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Encrypt
+{
+    partial class EncryptDecryptForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.UserEncrypt = new System.Windows.Forms.Label();
+            this.txtHaskkey = new System.Windows.Forms.TextBox();
+            this.lblHashKey = new System.Windows.Forms.Label();
+            this.PwdEncrypt = new System.Windows.Forms.Label();
+            this.lblUserEncryptMessage = new System.Windows.Forms.Label();
+            this.lblPwdEncryptMessage = new System.Windows.Forms.Label();
+            this.lblErrorMessage = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(165, 40);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(138, 20);
+            this.txtUserName.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(165, 63);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(138, 20);
+            this.txtPassword.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Location = new System.Drawing.Point(165, 114);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(64, 23);
+            this.btnEncrypt.TabIndex = 4;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Location = new System.Drawing.Point(235, 114);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(68, 23);
+            this.btnDecrypt.TabIndex = 5;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // UserEncrypt
+            // 
+            this.UserEncrypt.AutoSize = true;
+            this.UserEncrypt.Location = new System.Drawing.Point(169, 163);
+            this.UserEncrypt.Name = "UserEncrypt";
+            this.UserEncrypt.Size = new System.Drawing.Size(0, 13);
+            this.UserEncrypt.TabIndex = 6;
+            this.UserEncrypt.Visible = false;
+            // 
+            // txtHaskkey
+            // 
+            this.txtHaskkey.Location = new System.Drawing.Point(165, 88);
+            this.txtHaskkey.Name = "txtHaskkey";
+            this.txtHaskkey.Size = new System.Drawing.Size(138, 20);
+            this.txtHaskkey.TabIndex = 7;
+            // 
+            // lblHashKey
+            // 
+            this.lblHashKey.AutoSize = true;
+            this.lblHashKey.Location = new System.Drawing.Point(43, 95);
+            this.lblHashKey.Name = "lblHashKey";
+            this.lblHashKey.Size = new System.Drawing.Size(50, 13);
+            this.lblHashKey.TabIndex = 8;
+            this.lblHashKey.Text = "HashKey";
+            // 
+            // PwdEncrypt
+            // 
+            this.PwdEncrypt.AutoSize = true;
+            this.PwdEncrypt.Location = new System.Drawing.Point(169, 185);
+            this.PwdEncrypt.Name = "PwdEncrypt";
+            this.PwdEncrypt.Size = new System.Drawing.Size(0, 13);
+            this.PwdEncrypt.TabIndex = 9;
+            this.PwdEncrypt.Visible = false;
+            // 
+            // lblUserEncryptMessage
+            // 
+            this.lblUserEncryptMessage.AutoSize = true;
+            this.lblUserEncryptMessage.Location = new System.Drawing.Point(49, 163);
+            this.lblUserEncryptMessage.Name = "lblUserEncryptMessage";
+            this.lblUserEncryptMessage.Size = new System.Drawing.Size(80, 13);
+            this.lblUserEncryptMessage.TabIndex = 10;
+            this.lblUserEncryptMessage.Text = "Encrypted User";
+            this.lblUserEncryptMessage.Visible = false;
+            // 
+            // lblPwdEncryptMessage
+            // 
+            this.lblPwdEncryptMessage.AutoSize = true;
+            this.lblPwdEncryptMessage.Location = new System.Drawing.Point(49, 185);
+            this.lblPwdEncryptMessage.Name = "lblPwdEncryptMessage";
+            this.lblPwdEncryptMessage.Size = new System.Drawing.Size(103, 13);
+            this.lblPwdEncryptMessage.TabIndex = 11;
+            this.lblPwdEncryptMessage.Text = "Encrypted password";
+            this.lblPwdEncryptMessage.Visible = false;
+            // 
+            // lblErrorMessage
+            // 
+            this.lblErrorMessage.AutoSize = true;
+            this.lblErrorMessage.Location = new System.Drawing.Point(62, 213);
+            this.lblErrorMessage.Name = "lblErrorMessage";
+            this.lblErrorMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblErrorMessage.TabIndex = 12;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(194, 143);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // EncryptDecryptForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 261);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lblErrorMessage);
+            this.Controls.Add(this.lblPwdEncryptMessage);
+            this.Controls.Add(this.lblUserEncryptMessage);
+            this.Controls.Add(this.PwdEncrypt);
+            this.Controls.Add(this.lblHashKey);
+            this.Controls.Add(this.txtHaskkey);
+            this.Controls.Add(this.UserEncrypt);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Name = "EncryptDecryptForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label UserEncrypt;
+        private System.Windows.Forms.TextBox txtHaskkey;
+        private System.Windows.Forms.Label lblHashKey;
+        private System.Windows.Forms.Label PwdEncrypt;
+        private System.Windows.Forms.Label lblUserEncryptMessage;
+        private System.Windows.Forms.Label lblPwdEncryptMessage;
+        private System.Windows.Forms.Label lblErrorMessage;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Encrypt/Encrypt/EncryptDecryptForm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Encrypt
+{
+    public partial class EncryptDecryptForm : Form
+    {
+        public EncryptDecryptForm()
+        {
+            InitializeComponent();
+        }
+        private static string securityKey = "XcblWebServiceMERIDNow";
+
+        /// <summary>
+        ///     This method used for encrypting password 
+        /// </summary>
+        /// <param name="stringToEncrypt">string - password </param>
+        /// <returns>string - encrypted password</returns>
+        public static string Encrypt(string stringToEncrypt, string securekey)
+        {
+            try
+            {
+                byte[] keyArray;
+                byte[] encryptArray = UTF8Encoding.UTF8.GetBytes(stringToEncrypt);
+
+                // This class used for encrypt secure key
+                MD5CryptoServiceProvider cryptoServiceProvideMD5 = new MD5CryptoServiceProvider();
+                keyArray = cryptoServiceProvideMD5.ComputeHash(UTF8Encoding.UTF8.GetBytes(securityKey));
+                cryptoServiceProvideMD5.Clear();
+
+                TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+                tripleDES.Key = keyArray;
+                tripleDES.Mode = CipherMode.ECB;
+                tripleDES.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cryToTransform = tripleDES.CreateEncryptor();
+                byte[] resultArray = cryToTransform.TransformFinalBlock(encryptArray, 0, encryptArray.Length);
+                tripleDES.Clear();
+
+                return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+
+            }
+            catch
+            {
+                return "Error: Securitykey does not match";
+            }
+
+        }
+
+        /// <summary>
+        ///     This method used for decrypting password     
+        /// </summary>
+        /// <param name="stringToDecrypt">string - encrypted password</param>
+        /// <returns>string - decrypted password</returns>
+        public static string Decrypt(string stringToDecrypt, string securekey)
+        {
+            try
+            {
+                byte[] keyArray;
+                stringToDecrypt = stringToDecrypt.Replace(" ", "+");
+                byte[] decryptArray = Convert.FromBase64String(stringToDecrypt);
+
+                // This class used for encrypt secure key
+                MD5CryptoServiceProvider cryptoServiceProvideMD5 = new MD5CryptoServiceProvider();
+                keyArray = cryptoServiceProvideMD5.ComputeHash(UTF8Encoding.UTF8.GetBytes(securityKey));
+                cryptoServiceProvideMD5.Clear();
+
+                TripleDESCryptoServiceProvider tripleDES = new TripleDESCryptoServiceProvider();
+                tripleDES.Key = keyArray;
+                tripleDES.Mode = CipherMode.ECB;
+                tripleDES.Padding = PaddingMode.PKCS7;
+
+                ICryptoTransform cryToTransform = tripleDES.CreateDecryptor();
+                byte[] resultArray = cryToTransform.TransformFinalBlock(decryptArray, 0, decryptArray.Length);
+                tripleDES.Clear();
+
+                return UTF8Encoding.UTF8.GetString(resultArray);
+            }
+            catch
+            {
+                return "Error: Securitykey does not match";
+            }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string userName = txtUserName.Text;
+            string password = txtPassword.Text;
+            string hashkey = txtHaskkey.Text;
+
+            if (string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(hashkey))
+            {
+                lblErrorMessage.Text = String.Format("{0} is required.", string.IsNullOrWhiteSpace(userName) ? "Username" : (string.IsNullOrWhiteSpace(password) ? "Password" : "HashKey"));
+                lblErrorMessage.Visible = true;
+                return;
+            }
+
+            lblErrorMessage.Text = string.Empty;
+            lblErrorMessage.Visible = false;
+
+            UserEncrypt.Text = Encrypt(userName, hashkey);
+            txtUserName.Text = UserEncrypt.Text;
+         
+            PwdEncrypt.Text = Encrypt(password, hashkey);
+            txtPassword.Text = PwdEncrypt.Text;
+
+            lblUserEncryptMessage.Text = "Encrypt User";
+            lblPwdEncryptMessage.Text = "Encrypt Pwd";
+            UserEncrypt.Visible = true;
+            lblUserEncryptMessage.Visible = true;
+            lblPwdEncryptMessage.Visible = true;
+            PwdEncrypt.Visible = true;
+        }
+
+        public void button2_Click(object sender, EventArgs e)
+        {
+            string userName = txtUserName.Text;
+            string password = txtPassword.Text;
+            string hashkey = txtHaskkey.Text;
+
+            if (string.IsNullOrWhiteSpace(userName) || string.IsNullOrWhiteSpace(password) || string.IsNullOrWhiteSpace(hashkey))
+            {
+                lblErrorMessage.Text = String.Format("{0} is required.", string.IsNullOrWhiteSpace(userName) ? "Username" : (string.IsNullOrWhiteSpace(password) ? "Password" : "HashKey"));
+                lblErrorMessage.Visible = true;
+                return;
+            }
+
+            lblErrorMessage.Text = string.Empty;
+            lblErrorMessage.Visible = false;
+
+            UserEncrypt.Text = Decrypt(userName, txtHaskkey.Text);
+            PwdEncrypt.Text = Decrypt(password, txtHaskkey.Text);
+            txtUserName.Text = UserEncrypt.Text;
+            txtPassword.Text = UserEncrypt.Text;
+
+            lblUserEncryptMessage.Text = "Decrypt User";
+            lblPwdEncryptMessage.Text = "Decrypt Pwd";
+
+            UserEncrypt.Visible = true;
+            lblUserEncryptMessage.Visible = true;
+            lblPwdEncryptMessage.Visible = true;
+            PwdEncrypt.Visible = true;
+        }
+
+        public void btnClear_Click(object sender, EventArgs e)
+        {
+            txtUserName.Text = "";
+            txtPassword.Text = "";
+            txtHaskkey.Text = "";
+            UserEncrypt.Text = "";
+            PwdEncrypt.Text = "";
+
+            lblUserEncryptMessage.Visible = false;
+            lblPwdEncryptMessage.Visible = false; 
+        }
+    }
+}
