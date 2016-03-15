1a5292c7443076b479017627ee79ee19239d1c38
ishan.malik <ishan.malik@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-03-15 14:03:26 +0000

    Schedular & Grid both are at home page....
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@51 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

--- M4PL/M4PL.Apln/M4PL.Apln/Controllers/HomeController.cs
+++ M4PL/M4PL.Apln/M4PL.Apln/Controllers/HomeController.cs
@@ -14,8 +14,9 @@ namespace M4PL_API.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-            return View(NorthwindDataProvider.GetCustomers());
+            //return View(NorthwindDataProvider.GetCustomers());
             //return View(GetData());
+            return View(new HomeModel(NorthwindDataProvider.GetCustomers(), GetScheduleData()));
         }
 
         public ActionResult MasterDetailMasterPartial()
@@ -31,7 +32,7 @@ namespace M4PL_API.Controllers
 
         public ActionResult SchedulerPartial()
         {
-            return PartialView("SchedulerPartial", GetData());
+            return PartialView("SchedulerPartial", GetScheduleData());
         }
 
         public ActionResult Report()
@@ -39,22 +40,9 @@ namespace M4PL_API.Controllers
             return View(NorthwindDataProvider.GetAllReports());
         }
 
-        private object GetData()
+        private SchedulerModel GetScheduleData()
         {
-            var model = new SchedulerModel
-            {
-                Appointments = new[]
-                {
-                    new AppointmentModel() { Id=1, Title="Meeting 1", Start=DateTime.Today, End = DateTime.Today.AddHours(2), Resource = 1},
-                    new AppointmentModel() { Id=1, Title="Meeting 2", Start=DateTime.Today, End = DateTime.Today.AddHours(3), Resource = 1}
-                },
-                Resources = new[]
-                {
-                    new ResourceModel() { Id = 1, Title = "Manager"}
-                }
-            };
-
-            return model;
+            return new SchedulerModel();
         }
 
     }
@@ -64,6 +52,22 @@ namespace M4PL_API.Controllers
     {
         public AppointmentModel[] Appointments { get; set; }
         public ResourceModel[] Resources { get; set; }
+
+        public SchedulerModel()
+        {
+            this.Appointments = new[]
+            {
+                new AppointmentModel() { Id=1, Title="Meeting 1", Start=DateTime.Today, End = DateTime.Today.AddHours(2), Resource = 1},
+                new AppointmentModel() { Id=1, Title="Meeting 2", Start=DateTime.Today, End = DateTime.Today.AddHours(3), Resource = 1},
+                new AppointmentModel() { Id=2, Title="Meeting 3", Start=DateTime.Today.AddDays(3), End = DateTime.Today.AddDays(3).AddHours(1), Resource = 2},
+                new AppointmentModel() { Id=2, Title="Meeting 4", Start=DateTime.Today.AddDays(3), End = DateTime.Today.AddDays(3).AddHours(3), Resource = 2}
+            };
+            this.Resources = new[]
+            {
+                new ResourceModel() { Id = 1, Title = "Manager"},
+                new ResourceModel() { Id = 2, Title = "Workers"}
+            };
+        }
     }
     [Serializable]
     public class AppointmentModel
@@ -80,4 +84,18 @@ namespace M4PL_API.Controllers
         public int Id { get; set; }
         public string Title { get; set; }
     }
+
+    [Serializable]
+    public class HomeModel
+    {
+        public IEnumerable GetCustomers { get; set; }
+        public SchedulerModel SchedulerModel { get; set; }
+
+        public HomeModel(IEnumerable getCustomers, SchedulerModel schedulerModel)
+        {
+            this.GetCustomers = getCustomers;
+            this.SchedulerModel = schedulerModel;
+        }
+    }
+
 }
