cb099f5eca091b227e98a7a2a7106c4b5f322f47
antony.joseph <antony.joseph@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-03-29 05:23:43 +0000

    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@58 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.Apln/M4PL.Apln/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using M4PL_Apln.Models;
+
+namespace M4PL_Apln.Controllers
+{
+    public class ContactController : Controller
+    {
+        //
+        // GET: /Contact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            Contact contact = new Contact();
+            return View(contact);
+        }
+
+    }
+}
