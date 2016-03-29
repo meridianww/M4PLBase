cbfa5deb2421e0847b17bfcf8e8571bcf7bbd1bd
antony.joseph <antony.joseph@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-03-29 05:37:04 +0000

    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@64 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.Apln/M4PL.Apln/Models/Contact.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M4PL_Apln.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Company { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string Address { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string PostalCode { get; set; }
+
+        //public int Attachment { get; set; }
+
+        public string BusinessPhone { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        public string HomePhone { get; set; }
+
+        public string Fax { get; set; }
+
+        public string Email { get; set; }
+
+        public string Notes { get; set; }
+    }
+}
---
+++ M4PL/M4PL.Apln/M4PL.Apln/Scripts/App/Contact.js
@@ -0,0 +1,25 @@
+﻿$(document).ready(function () {
+	$("#saveContact").on('click', function () {
+		var contact = {};
+		contact.FirtstName = $("#firstName").val().trim();
+		contact.LastName = $("#lastName").val().trim();
+		contact.Company = $("#company").val().trim();
+		contact.JobTitle = $("#jobTitle").val().trim();
+		contact.Address = $("#address").val().trim();
+		contact.City = $("#city").val().trim();
+		contact.State = $("#state").val().trim();
+		contact.Country = $("#country").val().trim();
+		contact.PostalCode = $("#postal").val().trim();
+
+		$.ajax({
+			url: apiUrl + 'Contact/SaveContact',
+			type: 'POST',
+			data: JSON.stringify(contact),
+			contentType: 'application/json'
+		}).done(function (data) {
+			alert("SaveContact success");
+		}).fail(function (data) {
+			alert("SaveContact failed");
+		});
+    });
+});
