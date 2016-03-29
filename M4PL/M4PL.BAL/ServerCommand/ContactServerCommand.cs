f466fe036dbae5f33093787d6ba570c670e4cd92
antony.joseph <antony.joseph@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-03-29 05:37:41 +0000

    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@65 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.BAL/ServerCommand/ContactServerCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using M4PL_API_CommonUtils.Model;
+using M4PL.DataAccess;
+
+namespace M4PL_BAL.ServerCommand
+{
+	public class ContactServerCommand
+	{
+		public ContactServerCommand()
+		{
+
+		}
+
+		public bool InsertContact(Contact contact)
+		{
+			ContactSqlCommand contactSqlCommand = new ContactSqlCommand();
+			return contactSqlCommand.InsertContactDetails(contact);
+		}
+	}
+}
