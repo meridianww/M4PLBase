2741979181c90381980fbe88156d1faea2b36814
antony.joseph <antony.joseph@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-03-29 05:26:28 +0000

    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@62 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.API/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using M4PL_API_CommonUtils.Model;
+using M4PL_BAL.ServerCommand;
+
+namespace M4PL_API.Controllers
+{
+	public class ContactController : ApiController
+	{
+		[HttpPost]
+		public bool SaveContact(Contact contact)
+		{
+			ContactServerCommand contactServerCommand = new ContactServerCommand();
+			return contactServerCommand.InsertContact(contact);
+		}
+	}
+}
