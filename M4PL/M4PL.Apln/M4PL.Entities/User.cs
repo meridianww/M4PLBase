298b3d7ec2c1021974fc3dc741e11fc2e5fee836
antony.joseph <antony.joseph@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-03-30 13:10:09 +0000

    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@87 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

--- M4PL/M4PL.Apln/M4PL.Entities/User.cs
+++ M4PL/M4PL.Apln/M4PL.Entities/User.cs
@@ -8,12 +8,18 @@ namespace M4PL.Entities
 {
     public class User
     {
-        public string Email { get; set; }
-
-        public string Password { get; set; }
-
         public bool RememberMe { get; set; }
-
         public bool IsValidUser { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+
+		public long SysUserID { get; set; }
+		public int SysUserContactID { get; set; }
+		public string SysScreenName { get; set; }
+		public string SysPassword { get; set; }
+		public string SysComments { get; set; }
+		public string SysStatusAccount { get; set; }
+		public string SysEnteredBy { get; set; }
+		public string SysDateChangedBy { get; set; }
     }
 }
