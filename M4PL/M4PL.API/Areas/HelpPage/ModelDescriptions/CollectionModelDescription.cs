e164301d15b594d09577d681be7f5ba6b15529b0
ishan.malik <ishan.malik@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-04-01 14:58:10 +0000

    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@106 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

--- M4PL/M4PL.API/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
+++
@@ -1,7 +0,0 @@
-namespace M4PL.API.Areas.HelpPage.ModelDescriptions
-{
-    public class CollectionModelDescription : ModelDescription
-    {
-        public ModelDescription ElementDescription { get; set; }
-    }
-}
