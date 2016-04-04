5f0d94aaa85d9ee92d4142fb1ac64597aeea101c
ishan.malik <ishan.malik@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-04-04 13:28:40 +0000

    Organization Grid is ready...
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@111 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

--- M4PL/M4PL.Apln/M4PL.Apln/Scripts/App/Organization.js
+++ M4PL/M4PL.Apln/M4PL.Apln/Scripts/App/Organization.js
@@ -1,10 +1,46 @@
 ﻿(function () {
 
+    var url = "";
+
     $("#frmOrganization").keyup(function (event) {
         if (event.keyCode === 13) {
             $("#btnSave").click();
             e.preventDefault();
         }
     });
+
+    $("#tblOrganizations").load(function () {
+        RefreshGrid();
+    });
+
+    function RefreshGrid() {
+        url = apiUrl + 'Organization';
+        $.ajax({
+            url: url,
+            type: 'GET',
+            contentType: 'application/json'
+        }).success(function (response) {
+            BindGrid(response);
+        }).error(function (response) {
+            alert('Some error');
+        });
+    }
+
+    function BindGrid(data) {
+        if (data !== null && data.length > 0) {
+            for (var i = (data.length - 1) ; i >= 0; i--) {
+                $('#tblOrganizations tBody').append('<tr>');
+                $('#tblOrganizations tBody').append('<td>' + data[i].OrgCode + '</td>');
+                $('#tblOrganizations tBody').append('<td>' + data[i].OrgTitle + '</td>');
+                $('#tblOrganizations tBody').append('<td>' + data[i].OrgGroup + '</td>');
+                $('#tblOrganizations tBody').append('<td>' + data[i].Status + '</td>');
+                $('#tblOrganizations tBody').append('</tr>');
+            }
+        }
+        else {
+            data = [];
+            $('#tblOrganizations tBody').append('<tr><td colspan="100%">No records found</td></tr>');
+        }
+    }
 
 }());
