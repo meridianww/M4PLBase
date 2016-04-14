6a34d033bd9b23edb9a5c19de2d72e4004cbbb72
ishan.malik <ishan.malik@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-04-14 12:47:55 +0000

    Add Organization with Validations
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@159 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

--- M4PL/M4PL.Entities/Organization.cs
+++ M4PL/M4PL.Entities/Organization.cs
@@ -1,28 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace M4PL.Entities
 {
-	public class Organization
-	{
+    public class Organization
+    {
         public Organization()
         {
             this.OrgSortOrder = 1;
             this.OrgStatus = "Active";
         }
-		public int OrganizationID { get; set; }
-		public string OrgCode { get; set; }
-		public string OrgTitle { get; set; }
-		public string OrgGroup { get; set; }
-		public int OrgSortOrder { get; set; }
-		public string OrgDesc { get; set; }
-		public string OrgStatus { get; set; }
-		//public DateTime OrgDateEntered { get; set; }
-		public string OrgEnteredBy { get; set; }
-		//public DateTime OrgDateChanged { get; set; }
-		public string OrgDateChangedBy { get; set; }
-	}
+        public int OrganizationID { get; set; }
+
+        [Required(ErrorMessage = "Organization Code is required")]
+        [MaxLength(25)]
+        public string OrgCode { get; set; }
+
+        [Required(ErrorMessage = "Organization Title is required")]
+        [MaxLength(50)]
+        public string OrgTitle { get; set; }
+
+        [Required(ErrorMessage = "Organization Group is required")]
+        [MaxLength(25)]
+        public string OrgGroup { get; set; }
+
+        [Required(ErrorMessage = "Sort Order is required")]
+        [Range(1, int.MaxValue, ErrorMessage = "Sort order doesn't allow 0")]
+        public int OrgSortOrder { get; set; }
+
+        public string OrgDesc { get; set; }
+
+        [Required(ErrorMessage = "Status is required")]
+        [MaxLength(20)]
+        public string OrgStatus { get; set; }
+
+        //public DateTime OrgDateEntered { get; set; }
+        [MaxLength(50)]
+        public string OrgEnteredBy { get; set; }
+        //public DateTime OrgDateChanged { get; set; }
+        [MaxLength(50)]
+        public string OrgDateChangedBy { get; set; }
+    }
 }
