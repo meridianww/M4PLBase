1dfeaba43376b904f16f0d478ad57b39e95795b6
ishan.malik <ishan.malik@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-04-19 12:48:42 +0000

    Edit Contact is completed
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@179 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

--- M4PL/M4PL.Apln/M4PL.Apln/Controllers/ContactController.cs
+++ M4PL/M4PL.Apln/M4PL.Apln/Controllers/ContactController.cs
@@ -15,33 +15,35 @@ namespace M4PL_Apln.Controllers
         //
         // GET: /Contact/
 
+        Contact obj = new Contact();
+
         public ActionResult Index()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult Create(Contact obj, FormCollection collection)
+        public ActionResult Create(Contact contact, FormCollection collection)
         {
             try
             {
                 HttpPostedFileBase file = Request.Files["ImageData"];
                 if (file != null && file.ContentLength > 0)
                 {
-                    obj.Image = new byte[] { };
+                    contact.Image = new byte[] { };
                     using (var binaryReader = new BinaryReader(file.InputStream))
                     {
-                        obj.Image = binaryReader.ReadBytes(file.ContentLength);
+                        contact.Image = binaryReader.ReadBytes(file.ContentLength);
                     }
                 }
-                if (API_Contact.SaveContact(obj) > 0)
+                if (API_Contact.SaveContact(contact) > 0)
                     return RedirectToAction("Index");
                 else
-                    return View(obj);
+                    return View(contact);
             }
             catch
             {
-                return View(obj);
+                return View(contact);
             }
         }
 
@@ -58,7 +60,7 @@ namespace M4PL_Apln.Controllers
 
         public ActionResult Create()
         {
-            return View();
+            return View(obj);
         }
 
         public ActionResult Delete(int Id)
@@ -67,6 +69,49 @@ namespace M4PL_Apln.Controllers
                 return RedirectToAction("Index");
             else
                 return null;
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            obj = API_Contact.GetContactDetails(Id);
+            if (obj != null)
+            {
+                obj.Image = obj.LstImages.ToArray();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Edit(int Id, Contact contact, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                if (Id > 0 && ModelState.IsValid)
+                {
+                    contact.ContactID = Id;
+                    HttpPostedFileBase file = Request.Files["ImageData"];
+                    if (file != null && file.ContentLength > 0)
+                    {
+                        contact.Image = new byte[] { };
+                        using (var binaryReader = new BinaryReader(file.InputStream))
+                        {
+                            contact.Image = binaryReader.ReadBytes(file.ContentLength);
+                        }
+                    }
+                    if (API_Contact.SaveContact(contact) > 0)
+                        return RedirectToAction("Index");
+                    else
+                        return View(contact);
+                }
+                else
+                    return View(contact);
+            }
+            catch
+            {
+                return View();
+            }
         }
 
     }
