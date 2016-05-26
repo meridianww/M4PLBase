e35ee886f9036325b758c05257a2c4660d303c66
ishan.malik <ishan.malik@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-05-26 08:52:15 +0000

    ErrorPage recreate
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@399 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.Apln/M4PL.Apln/Controllers/ErrorPageController.cs
@@ -0,0 +1,33 @@
+﻿//Copyright (2016) Meridian Worldwide Transportation Group
+//All Rights Reserved Worldwide
+//====================================================================================================================================================
+//Program Title:                                Meridian 4th Party Logistics(M4PL)
+//Programmer:                                   Janardana
+//Date Programmed:                              28/3/2016
+//Program Name:                                 Login
+//Purpose:                                      Providing view for logging into the application
+//
+//==================================================================================================================================================== 
+
+using M4PL.APIClient;
+using M4PL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M4PL_Apln.Controllers
+{
+    public class ErrorPageController : Controller
+    {
+        //
+        // GET: /ErrorPage/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
