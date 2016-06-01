29d54758ee1ceea6e9b56bc5f4cb6449f3053d00
ishan.malik <ishan.malik@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-06-01 12:36:54 +0000

    Handle all the Error StatusCode in Exception Filter.
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@472 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

--- M4PL/M4PL.Apln/M4PL.Apln/App_Start/HandleResourceNotFoundAttribute.cs
+++ M4PL/M4PL.Apln/M4PL.Apln/App_Start/HandleResourceNotFoundAttribute.cs
@@ -30,23 +30,80 @@ namespace M4PL_Apln.App_Start
             if (exception is TargetInvocationException)
                 exception = exception.InnerException;
 
-            BAL_ErrorLog.LogException(new LogError
-            {
-                UserName = "",
-                ApplicationUrl = HttpContext.Current.Request.Url + Environment.NewLine,
-                Message = exception.Message != null ? exception.Message.ToString(CultureInfo.InvariantCulture) : "",
-                Source = exception.Source != null ? exception.Source.ToString(CultureInfo.InvariantCulture) : "",
-                StackTrace = exception.StackTrace != null ? exception.StackTrace.ToString(CultureInfo.InvariantCulture) : ""
-            });
-
             context.ExceptionHandled = true;
             context.HttpContext.Response.Clear();
             context.HttpContext.Response.StatusCode = 404;
 
-            var routeData = new RouteData();
-            routeData.Values["controller"] = "ErrorPage";
-            routeData.Values["action"] = "Index";
-            ((IController)new ErrorPageController()).Execute(new RequestContext(new HttpContextWrapper(HttpContext.Current), routeData));
+            this.ErrorLog(exception);
+        }
+
+        void ErrorLog(Exception exception)
+        {
+            var httpContext = HttpContext.Current;
+            var currentController = "";
+            var currentAction = "Index";
+            var currentRouteData = RouteTable.Routes.GetRouteData(new HttpContextWrapper(httpContext));
+
+            if (currentRouteData != null)
+            {
+                if (currentRouteData.Values["controller"] != null && !string.IsNullOrEmpty(currentRouteData.Values["controller"].ToString()))
+                    currentController = currentRouteData.Values["controller"].ToString();
+
+                if (currentRouteData.Values["action"] != null && !string.IsNullOrEmpty(currentRouteData.Values["action"].ToString()))
+                    currentAction = currentRouteData.Values["action"].ToString();
+
+                var controller = new ErrorPageController();
+                var routeData = new RouteData();
+                var action = "Index";
+
+                if (exception is HttpException)
+                {
+                    var httpEx = exception as HttpException;
+                    switch (httpEx.GetHttpCode())
+                    {
+                        case 400:
+                            action = "Error400";
+                            break;
+                        case 401:
+                            action = "Error401";
+                            break;
+                        case 500:
+                            action = "Error500";
+                            break;
+                        case 403:
+                            action = "Error403";
+                            break;
+                        case 404:
+                            action = "Error404";
+                            break;
+                        default:
+                            action = "Index";
+                            break;
+                    }
+                }
+
+                httpContext.ClearError();
+                httpContext.Response.Clear();
+                httpContext.Response.StatusCode = exception is HttpException ? ((HttpException)exception).GetHttpCode() : 500;
+                httpContext.Response.TrySkipIisCustomErrors = true;
+
+                routeData.Values["controller"] = "ErrorPage";
+                routeData.Values["action"] = action;
+
+                if (exception != null)
+                {
+                    BAL_ErrorLog.LogException(new LogError
+                    {
+                        UserName = "",
+                        ApplicationUrl = HttpContext.Current.Request.Url + Environment.NewLine,
+                        Message = exception.Message != null ? exception.Message.ToString(CultureInfo.InvariantCulture) : "",
+                        Source = exception.Source != null ? exception.Source.ToString(CultureInfo.InvariantCulture) : "",
+                        StackTrace = exception.StackTrace != null ? exception.StackTrace.ToString(CultureInfo.InvariantCulture) : ""
+                    });
+                    controller.ViewData.Model = new HandleErrorInfo(exception ?? new Exception(), currentController, currentAction);
+                }
+                ((IController)controller).Execute(new RequestContext(new HttpContextWrapper(httpContext), routeData));
+            }
 
         }
     }
