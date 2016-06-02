7ba369df408d9b5b93c790b04938509e73d376f0
ram.kumar <ram.kumar@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-06-02 11:27:00 +0000

    System Messages  Template - Web Api Logic
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@487 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.API/Controllers/SystemMessagesController.cs
@@ -0,0 +1,45 @@
+﻿
+//Copyright (2016) Meridian Worldwide Transportation Group
+//All Rights Reserved Worldwide
+//====================================================================================================================================================
+//Program Title:                                Meridian 4th Party Logistics(M4PL)
+//Programmer:                                   Ramkumar 
+//Date Programmed:                              2/6/2016
+//Program Name:                                 System Message Template
+//Purpose:                                      Returns serialized data for System Message Template
+//
+//==================================================================================================================================================== 
+
+
+using M4PL.Entities;
+using M4PL_API_CommonUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using M4PL_BAL;
+
+namespace M4PL.API.Controllers
+{
+    public class SystemMessagesController : Controller
+    {  
+
+        /// <summary>
+        /// Function to get the list of all SystemMessages
+        /// </summary>
+        /// <returns></returns>
+        public Response<disMessages> Get()
+        {
+            try
+            {
+                return new Response<disMessages> { Status = true, DataList = BAL_SysMessages.GetSysMessagesTemplates() ?? new List<disMessages>()};
+            }
+            catch (Exception ex)
+            {
+                return new Response<disMessages> { Status = false, MessageType = MessageTypes.Exception, Message = ex.Message };
+            }
+        }
+               
+    }
+}
