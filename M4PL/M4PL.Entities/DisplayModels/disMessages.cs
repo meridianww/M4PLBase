adabc8fe9fc4d2618d6248f88d7c9b959169cf77
ram.kumar <ram.kumar@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-06-02 11:26:21 +0000

    System Messages  Template - Business logic
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@486 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.Entities/DisplayModels/disMessages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M4PL.Entities
+{
+    public class disMessages
+    {
+        //SYSMS000Master table columns
+        public long SysMessageID { get; set; }
+        public int SysMsgType { get; set; }
+        public int SysMessageCode { get; set; }
+        public string sysMessageScreenTitle { get; set; }
+        public string SysMessageTitle { get; set; }
+        public string SysMessageInstruction { get; set; }       
+        public string SysMessageDescription { get; set; }
+        public short SysMessageButtonSelection { get; set; }
+        public string SysLanguageCode { get; set; }
+        public DateTime SysMessageDateEntered { get; set; }
+        public string SysMessageEnteredBy { get; set; }
+        public DateTime SysMessageDateChanged { get; set; }
+        public string SysMessageDateChangedBy { get; set; }
+
+        //SYSMS010Ref_MessageTypes table column
+        public string SysMsgTypeIcon { get; set; }
+        
+    }
+}
+
