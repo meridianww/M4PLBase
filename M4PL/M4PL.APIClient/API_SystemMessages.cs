b34b8e9adfbdce6c63bd702d41f134213b227221
ram.kumar <ram.kumar@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-06-02 12:02:41 +0000

    System Messages  Template - Web Api Logic
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@488 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.APIClient/API_SystemMessages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using M4PL.Entities;
+using M4PL_API_CommonUtils;
+using RestSharp;
+using Newtonsoft.Json;
+
+namespace M4PL.APIClient
+{
+    public class API_SystemMessages
+    {
+        /// <summary>
+        /// Function to get all Contacts data
+        /// </summary>
+        /// <returns></returns>
+        public static Response<disMessages> GetSysMessagesTemplates()
+        {
+            RestClient _client = new RestClient { BaseUrl = new Uri(M4PL_Constants.M4PL_API) };
+            var request = new RestRequest("SystemMessages", Method.GET) { RequestFormat = DataFormat.Json };
+            var response = _client.Execute<Response<disMessages>>(request);
+            if (response.Data == null)
+                return new Response<disMessages> { Status = false, MessageType = MessageTypes.Exception, Message = response.ErrorMessage };
+            return response.Data;
+
+        }
+
+      
+    }
+}
