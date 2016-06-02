ca0e801d6d61c8b2425efde090f016d7628f88af
ram.kumar <ram.kumar@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2016-06-02 11:08:46 +0000

    System Messages  Template - Business logic
    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@484 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL/M4PL.API.DAL/DAL/DAL_SysMessages.cs
@@ -0,0 +1,36 @@
+﻿//Copyright (2016) Meridian Worldwide Transportation Group
+//All Rights Reserved Worldwide
+//====================================================================================================================================================
+//Program Title:                                Meridian 4th Party Logistics(M4PL)
+//Programmer:                                   Ramkumar 
+//Date Programmed:                              2/6/2016
+//Program Name:                                 System Messages
+//Purpose:                                      Create, access, and review data from database for System Messages
+//
+//==================================================================================================================================================== 
+
+using M4PL.DataAccess.Serializer;
+using M4PL.Entities;
+using M4PL_API_CommonUtils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M4PL.DataAccess.DAL
+{
+    public class DAL_SysMessages
+    {
+      
+
+        /// <summary>
+        /// Function to get System Predefined Messages from the DB
+        /// </summary>
+        /// <returns></returns>
+        public static List<disMessages> GetSysMessagesTemplates()
+        {
+            return SqlSerializer.Default.DeserializeMultiRecords<disMessages>(StoredProcedureNames.GetSysMessagesTemplates,  new Parameter[] { }, false, true);
+        }
+    }
+}
