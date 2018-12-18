d2bb937c695423de17e631b1d59ef974dc3ec906
dharmendra.verma <dharmendra.verma@80c1dc9e-45f0-9747-8a74-c6ecc499fc54>
2018-12-18 16:20:45 +0000

    git-svn-id: https://meridianww.cloudapp.net/svn/M4PL/trunk@5419 80c1dc9e-45f0-9747-8a74-c6ecc499fc54

---
+++ M4PL.XCBL.WebService/XCBL.WebService/XCBL.SoapWebService.Phase2/xCBLSoapWebService/PBSData.cs
@@ -0,0 +1,10 @@
+﻿namespace xCBLSoapWebService
+{
+    public class PBSData
+    {
+        public string ScheduleId { get; set; }
+        public string OrderNumber { get; set; }
+        public string Status { get; set; }
+        public string Comment { get; set; }
+    }
+}
