e3fb362d84199e02daae18072e382dbd295f743f
Prashant Aggarwal <prashant.aggarwal@globaltranz.com>
2019-09-11 14:50:32 +0530

    Commit the changes For Survey

---
+++ QA/M4PL/M4PL.API/IoC/SurveyModule.cs
@@ -0,0 +1,24 @@
+﻿/*Copyright (2016) Meridian Worldwide Transportation Group
+//All Rights Reserved Worldwide
+//====================================================================================================================================================
+//Program Title:                                Meridian 4th Party Logistics(M4PL)
+//Programmer:                                   Akhil
+//Date Programmed:                              10/10/2017
+//Program Name:                                 AdminModule
+//Purpose:                                      For implementing IOC in Admin module
+//====================================================================================================================================================*/
+
+using Autofac;
+using M4PL.Business.Survey;
+
+namespace M4PL.API.IoC
+{
+    public class SurveyModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<JobSurveyCommands>().As<IJobSurveyCommands>().InstancePerRequest();
+			base.Load(builder);
+        }
+    }
+}
