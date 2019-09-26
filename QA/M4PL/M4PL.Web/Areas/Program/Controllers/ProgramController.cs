d47916d5fce05226690897ca99ecef7c48ccb972
Prashant Aggarwal <prashant.aggarwal@globaltranz.com>
2019-09-26 16:28:52 +0530

    Commit Rich Editor Changes

--- QA/M4PL/M4PL.Web/Areas/Program/Controllers/ProgramController.cs
+++ QA/M4PL/M4PL.Web/Areas/Program/Controllers/ProgramController.cs
@@ -108,15 +108,10 @@ namespace M4PL.Web.Areas.Program.Controllers
 
 		#region RichEdit
 
-		public ActionResult RichEditDescription(string strRoute, M4PL.Entities.Support.Filter docId)
+		public ActionResult RichEditDescription(string strRoute)
 		{
-			long newDocumentId;
 			var route = JsonConvert.DeserializeObject<MvcRoute>(strRoute);
 			var byteArray = route.GetVarbinaryByteArray(ByteArrayFields.PrgDescription.ToString());
-			if (docId != null && docId.FieldName.Equals("ArbRecordId") && long.TryParse(docId.Value, out newDocumentId))
-			{
-				byteArray = route.GetVarbinaryByteArray(newDocumentId, ByteArrayFields.PrgDescription.ToString());
-			}
 			if (route.RecordId > 0)
 				byteArray.Bytes = _commonCommands.GetByteArrayByIdAndEntity(byteArray).Bytes;
 			return base.RichEditFormView(byteArray);
