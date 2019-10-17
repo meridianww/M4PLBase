7a055fabc624a9398272191c5a6d85db8c8f2fd3
ChauhanNikhil <nikhil.chauhan@dreamorbit.com>
2019-10-17 16:19:46 +0530

    Removed Script

--- QA/Database/Scripts/Sprint117/#46474/46488_ScriptToUpdateRefTable.sql
+++
@@ -1,2 +0,0 @@
-update [dbo].[SYSTM000Ref_Table] set TblTableName = 'CONTC010Bridge'  ,TblParentIdFieldName = 'ConPrimaryRecordId' ,TblItemNumberFieldName = 'ConItemNumber'  where  SysRefName  IN ('VendDcLocation','CustDcLocation')
- 
