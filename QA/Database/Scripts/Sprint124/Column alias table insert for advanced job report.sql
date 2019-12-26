e3e521375e6428fb66727a8aced5bf5ec91e6beb
KamalAdhikary <kamal.adhikary@dreamorbit.com>
2019-12-26 22:54:12 +0530

    Column alias for job advanced report

---
+++ QA/Database/Scripts/Sprint124/Column alias table insert for advanced job report.sql
@@ -0,0 +1,55 @@
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'CustomerId', 'Customer Id', 'Customer Id', NULL, NULL, NULL, 1, 1, 0, 1, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'ProgramId', 'ProgramId', 'Program ID', NULL, NULL, NULL, 2, 0, 1, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'OrderTypeId', 'OrderTypeId', 'OrderType ID', NULL, NULL, NULL, 3, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'StartDate', 'StartDate', 'Start Date', NULL, NULL, NULL, 4, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'EndDate', 'EndDate', 'End Date', NULL, NULL, NULL, 5, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'Scheduled', 'Scheduled', 'Scheduled', NULL, NULL, NULL, 6, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'Origin', 'Origin', 'Origin', NULL, NULL, NULL, 7, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'Destination', 'Destination', 'Destination', NULL, NULL, NULL, 8, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'JobStatusId', 'JobStatusId', 'Job StatusId', NULL, NULL, NULL, 9, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'GatewayStatusId', 'GatewayStatusId', 'Gateway StatusId', NULL, NULL, NULL, 10, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'ServiceMode', 'ServiceMode', 'Service Mode', NULL, NULL, NULL, 11, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'Mode', 'Mode', 'Mode', NULL, NULL, NULL, 12, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+INSERT INTO dbo.SYSTM000ColumnsAlias (LangCode, ColTableName, ColAssociatedTableName, ColColumnName, ColAliasName, ColCaption, ColLookupId, ColLookupCode, ColDescription, ColSortOrder, ColIsReadOnly, ColIsVisible, ColIsDefault, StatusId, ColDisplayFormat, ColAllowNegativeValue, ColIsGroupBy, ColMask)
+VALUES ('EN', 'JobAdvanceReport', NULL, 'Search', 'Search', 'Search', NULL, NULL, NULL, 13, 0, 0, 0, 1, NULL, 0, 0, NULL)
+GO
+
+
+
+
