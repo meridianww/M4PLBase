8e9e879f096fe96d98f2a868b33d4ae41032b5a3
prasanta.mahankuda <prasanta.mahankuda@dreamorbit.com>
2020-02-22 17:33:53 +0530

    Grid populate issue fixed

--- QA/Database/Stored Procedures/dbo.GetJobCardView.sql
+++ QA/Database/Stored Procedures/dbo.GetJobCardView.sql
@@ -92,12 +92,13 @@ BEGIN TRY
 	BEGIN
 		IF (@recordId = 0)
 		BEGIN
-			 Declare @QueryData Varchar(Max)
-			 Select @QueryData = [dbo].[fnGetGridBaseQueryByUserId](@entity, @userId)
-			 SELECT @QueryData = REPLACE(@QueryData, 'JobCard.JobPartsActual', 'CASE WHEN ISNULL(JobCard.JobPartsActual, 0) > 0 THEN CAST(JobCard.JobPartsActual AS INT)  ELSE 0 END JobPartsActual');
-			 SELECT @QueryData =  REPLACE(@QueryData, 'JobCard.JobQtyActual', 'CASE WHEN ISNULL(JobCard.JobQtyActual, 0) > 0 THEN CAST(JobCard.JobQtyActual AS INT)  ELSE 0 END JobQtyActual');
-			 SELECT @QueryData =  REPLACE(@QueryData, 'JobCard.JobPartsOrdered', 'CAST(JobCard.JobPartsOrdered AS INT) JobPartsOrdered');  
-			 SET @sqlCommand = 'SELECT  DISTINCT  ' + @QueryData  
+			Declare @QueryData Varchar(Max)
+			Select @QueryData = [dbo].[fnGetGridBaseQueryByUserId](@entity, @userId)
+			SELECT @QueryData = REPLACE(@QueryData, 'JobCard.JobPartsActual', 'CASE WHEN ISNULL(JobCard.JobPartsActual, 0) > 0 THEN CAST(JobCard.JobPartsActual AS INT)  ELSE NULL END JobPartsActual');
+            SELECT @QueryData =  REPLACE(@QueryData, 'JobCard.JobQtyActual', 'CASE WHEN ISNULL(JobCard.JobQtyActual, 0) > 0 THEN CAST(JobCard.JobQtyActual AS INT) ELSE NULL END JobQtyActual');
+            SELECT @QueryData =  REPLACE(@QueryData, 'JobCard.JobPartsOrdered', 'CAST(JobCard.JobPartsOrdered AS INT) JobPartsOrdered');  
+
+			SET @sqlCommand = 'SELECT DISTINCT ' + @QueryData      
 
 		END
 		ELSE
--- QA/M4PL/M4PL.APIClient/ViewModels/Job/JobCardView.cs
+++ QA/M4PL/M4PL.APIClient/ViewModels/Job/JobCardView.cs
@@ -8,6 +8,5 @@ namespace M4PL.APIClient.ViewModels.Job
 {
     public class JobCardView : Entities.Job.JobCard
     {
-        public string ControlNamePrefix { get; set; }
     }
 }
