c8660bd6c464f179e5e1940820440b222f645106
Prashant Aggarwal <prashant.aggarwal@dreamorbit.com>
2020-03-18 16:09:48 +0530

    Commit the Color Changes

--- QA/Database/Stored Procedures/dbo.GetJobCardView.sql
+++ QA/Database/Stored Procedures/dbo.GetJobCardView.sql
@@ -142,6 +142,10 @@ SELECT @JobCount = Count(ISNULL(EntityId, 0))
 			SELECT @QueryData = REPLACE(@QueryData, 'JobCard.JobPartsActual', 'CASE WHEN ISNULL(JobCard.JobPartsActual, 0) > 0 THEN CAST(JobCard.JobPartsActual AS INT)  ELSE NULL END JobPartsActual');
             SELECT @QueryData =  REPLACE(@QueryData, 'JobCard.JobQtyActual', 'CASE WHEN ISNULL(JobCard.JobQtyActual, 0) > 0 THEN CAST(JobCard.JobQtyActual AS INT) ELSE NULL END JobQtyActual');
             SELECT @QueryData =  REPLACE(@QueryData, 'JobCard.JobPartsOrdered', 'CAST(JobCard.JobPartsOrdered AS INT) JobPartsOrdered');  
+			SELECT @QueryData = @QueryData + ', CASE WHEN DATEDIFF(ss, JobCard.JobOriginDateTimePlanned , GETUTCDATE()) <= 172800 THEN ''#FF0000''
+			                          WHEN DATEDIFF(ss, JobCard.JobOriginDateTimePlanned , GETUTCDATE()) > 172800 
+									    AND DATEDIFF(ss, JobCard.JobOriginDateTimePlanned , GETUTCDATE()) <= 432000 THEN ''#FFFF00''
+									  WHEN DATEDIFF(ss, JobCard.JobOriginDateTimePlanned , GETUTCDATE()) > 432000 THEN ''#008000'' END AS JobColorCode'
 
 			SET @sqlCommand = 'SELECT DISTINCT ' + @QueryData      
 
