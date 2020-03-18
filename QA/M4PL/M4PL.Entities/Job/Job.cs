5a32c345c7dc8d9ce407128bbca08b9d8fd8e659
kamal.adhikary <kamal.adhikary@dreamorbit.com>
2020-03-18 15:11:16 +0530

    #color code

--- QA/M4PL/M4PL.Entities/Job/Job.cs
+++ QA/M4PL/M4PL.Entities/Job/Job.cs
@@ -553,6 +553,6 @@ namespace M4PL.Entities.Job
         public int? JobPreferredMethod { get; set; }
         public string JobPreferredMethodName { get; set; }
         public decimal JobMileage { get; set; }
-        
+        public string JobColorCode { get; set; }
     }
 }
