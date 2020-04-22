eac1447e3bd069301cb2b9392de6dfcacc787693
Prashant Aggarwal <prashant.aggarwal@dreamorbit.com>
2020-04-22 17:26:33 +0530

    Commit xCBL Changes

---
+++ QA/M4PL/M4PL.Business/XCBL/ElectroluxOrderMapping/JobASNDataMapper.cs
@@ -0,0 +1,26 @@
+﻿using M4PL.Entities.XCBL.Electrolux.OrderRequest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M4PL.Business.XCBL.ElectroluxOrderMapping
+{
+	public class JobASNDataMapper
+	{
+		public Entities.Job.Job ToJobASNModel(OrderHeader orderHeader, ref Entities.Job.Job existingJobData)
+		{
+			if (orderHeader?.ASNdata == null) return existingJobData;
+			var asnData = orderHeader.ASNdata;
+			existingJobData.JobCarrierContract = asnData.VehicleId;
+			existingJobData.JobBOL = asnData.BolNumber;
+			existingJobData.JobShipmentDate = (!string.IsNullOrEmpty(asnData.Shipdate) && asnData.Shipdate.Length >= 8)
+				? (DateTime?)Convert.ToDateTime(string.Format("{0}-{1}-{2}", asnData.Shipdate.Substring(0, 4), asnData.Shipdate.Substring(4, 2), asnData.Shipdate.Substring(6, 2)))
+				: existingJobData.JobShipmentDate;
+
+			return existingJobData;
+		}
+
+	}
+}
---
+++ QA/M4PL/M4PL.Utilities/UtilityExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace M4PL.Utilities
+{
+    public static class UtilityExtensions
+    {
+        public static decimal ToDecimal(this string value)
+        {
+            decimal returnValue;
+
+            return decimal.TryParse(value, out returnValue) ? returnValue : 0m;
+        }
+
+        public static byte ToByte(this string value)
+        {
+            byte returnValue;
+
+            return byte.TryParse(value, out returnValue) ? returnValue : (byte)0;
+        }
+
+        public static int ToInt(this string value)
+        {
+            int returnValue;
+
+            return int.TryParse(value, out returnValue) ? returnValue : 0;
+        }
+
+        public static DateTime? ToDate(this string dateValue)
+        {
+            DateTime value;
+
+            if (DateTime.TryParse(dateValue, out value))
+            {
+                return value;
+            }
+
+            return null;
+        }
+    }
+}
