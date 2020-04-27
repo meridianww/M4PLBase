9519167443789f2ea138333fe43e1eb00e7bc473
Prashant Aggarwal <prashant.aggarwal@dreamorbit.com>
2020-04-27 12:18:01 +0530

    Commit Electrolux Delivery Update

---
+++ QA/M4PL/M4PL.Business/XCBL/HelperClasses/ElectroluxHelper.cs
@@ -0,0 +1,102 @@
+﻿using M4PL.Entities.Support;
+using M4PL.Entities.XCBL.Electrolux.DeliveryUpdateRequest;
+using M4PL.Entities.XCBL.Electrolux.DeliveryUpdateResponse;
+using M4PL.Utilities.Logger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using _logger = M4PL.DataAccess.Logger.ErrorLogger;
+
+namespace M4PL.Business.XCBL.HelperClasses
+{
+	public static class ElectroluxHelper
+	{
+		public static DeliveryUpdateResponse SendDeliveryUpdateRequestToElectrolux(ActiveUser activeUser, DeliveryUpdate deliveryUpdate, long jobId)
+		{
+			DeliveryUpdateResponse deliveryUpdateResponse = null;
+			string deliveryUpdateXml = string.Empty;
+			string deliveryUpdateURL = string.Empty;
+			string deliveryUpdateUserName = string.Empty;
+			string deliveryUpdatePassword = string.Empty;
+			if (!M4PBusinessContext.ComponentSettings.IsElectroluxDeliveryUpdateProduction)
+			{
+				deliveryUpdateURL = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateTestAPIUrl;
+				deliveryUpdateUserName = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateTestAPIUsername;
+				deliveryUpdatePassword = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateTestAPIPassword;
+			}
+			else
+			{
+				deliveryUpdateURL = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateProductionAPIUrl;
+				deliveryUpdateUserName = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateProductionAPIUsername;
+				deliveryUpdatePassword = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateProductionAPIPassword;
+			}
+
+			try
+			{
+				NetworkCredential networkCredential = new NetworkCredential(deliveryUpdateUserName, deliveryUpdatePassword);
+				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(deliveryUpdateURL);
+				request.Credentials = networkCredential;
+				request.KeepAlive = false;
+				request.ContentType = "application/xml";
+				request.Method = "POST";
+				using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+				{
+					deliveryUpdateXml = CreateDeliveryUpdateRequestXml(deliveryUpdate);
+					streamWriter.Write(deliveryUpdateXml);
+				}
+
+				WebResponse response = request.GetResponse();
+
+				using (Stream electroluxDeliveryUpdateResponseStream = response.GetResponseStream())
+				{
+					using (TextReader electroluxDeliveryUpdateResponseReader = new StreamReader(electroluxDeliveryUpdateResponseStream))
+					{
+						string electroluxDeliveryUpdateResponseString = electroluxDeliveryUpdateResponseReader.ReadToEnd();
+
+						using (var stringReader = new StringReader(electroluxDeliveryUpdateResponseString))
+						{
+							deliveryUpdateResponse = GenerateDeliveryUpdateResponseFromString(electroluxDeliveryUpdateResponseString);
+						}
+					}
+				}
+			}
+			catch (Exception exp)
+			{
+				_logger.Log(exp, string.Format("Error is occuring while Sending the Delivery Update To Electrolux: Request Url is: {0}, Request body xml was {1}", deliveryUpdateURL, deliveryUpdateXml), string.Format("Electrolux delivery update for JobId: {0}", jobId), LogType.Error);
+			}
+
+			return deliveryUpdateResponse;
+		}
+		private static string CreateDeliveryUpdateRequestXml(DeliveryUpdate deliveryUpdate)
+		{
+			XmlDocument xmlDoc = new XmlDocument();
+			XmlSerializer xmlSerializer = new XmlSerializer(deliveryUpdate.GetType());
+			using (MemoryStream xmlStream = new MemoryStream())
+			{
+				xmlSerializer.Serialize(xmlStream, deliveryUpdate);
+				xmlStream.Position = 0;
+				xmlDoc.Load(xmlStream);
+				return string.Format(format: "{0} {1} {2}", arg0: "<ns:DeliveryUpdate xmlns:ns=\"http://esb.electrolux.com/FinalMile/Delivery\">", arg1: xmlDoc.DocumentElement.InnerXml, arg2: "</ns:DeliveryUpdate>");
+			}
+		}
+		private static DeliveryUpdateResponse GenerateDeliveryUpdateResponseFromString(string updateResponseString)
+		{
+			updateResponseString = updateResponseString.Replace("NS1:DeliveryUpdateResponse", "DeliveryUpdateResponse");
+			DeliveryUpdateResponse deliveryUpdateResponse = null;
+			XmlSerializer serializer = new XmlSerializer(typeof(DeliveryUpdateResponse));
+			using (var stringReader = new StringReader(updateResponseString))
+			{
+				deliveryUpdateResponse = (DeliveryUpdateResponse)serializer.Deserialize(stringReader);
+			}
+
+			return deliveryUpdateResponse;
+		}
+
+	}
+}
--- QA/M4PL/M4PL.Business/XCBL/XCBLCommands.cs
+++ QA/M4PL/M4PL.Business/XCBL/XCBLCommands.cs
@@ -5,26 +5,20 @@ using M4PL.Entities.XCBL.Electrolux.OrderResponse;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using _commands = M4PL.DataAccess.XCBL.XCBLCommands;
 using _jobCommands = M4PL.DataAccess.Job.JobCommands;
-using _logger = M4PL.DataAccess.Logger.ErrorLogger;
 using M4PL.Entities.Support;
 using M4PL.Entities.Job;
 using M4PL.Business.XCBL.ElectroluxOrderMapping;
 using M4PL.Utilities;
 using M4PL.Entities.XCBL.Electrolux.DeliveryUpdateResponse;
 using M4PL.Entities.XCBL.Electrolux.DeliveryUpdateRequest;
-using System.Xml;
-using System.Xml.Serialization;
-using System.IO;
-using System.Net;
-using M4PL.Utilities.Logger;
+using M4PL.Business.XCBL.HelperClasses;
 
 namespace M4PL.Business.XCBL
 {
-    public class XCBLCommands : BaseCommands<Entities.XCBL.XCBLToM4PLRequest>, IXCBLCommands
+	public class XCBLCommands : BaseCommands<XCBLToM4PLRequest>, IXCBLCommands
     {
 		#region Public Methods
 
@@ -142,7 +136,7 @@ namespace M4PL.Business.XCBL
 
 		public DeliveryUpdateResponse ProcessElectroluxOrderDeliveryUpdate(DeliveryUpdate deliveryUpdate, long jobId)
 		{
-			return SendDeliveryUpdateRequestToElectrolux(ActiveUser, deliveryUpdate, jobId);
+			return ElectroluxHelper.SendDeliveryUpdateRequestToElectrolux(ActiveUser, deliveryUpdate, jobId);
 		}
 
 		#endregion
@@ -533,87 +527,7 @@ namespace M4PL.Business.XCBL
             return request;
 
         }
-		private DeliveryUpdateResponse SendDeliveryUpdateRequestToElectrolux(ActiveUser activeUser, DeliveryUpdate deliveryUpdate, long jobId)
-		{
-			DeliveryUpdateResponse deliveryUpdateResponse = null;
-			string deliveryUpdateXml = string.Empty;
-			string deliveryUpdateURL = string.Empty;
-			string deliveryUpdateUserName = string.Empty;
-			string deliveryUpdatePassword = string.Empty;
-			if (!M4PBusinessContext.ComponentSettings.IsElectroluxDeliveryUpdateProduction)
-			{
-				deliveryUpdateURL = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateTestAPIUrl;
-				deliveryUpdateUserName = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateTestAPIUsername;
-				deliveryUpdatePassword = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateTestAPIPassword;
-			}
-			else
-			{
-				deliveryUpdateURL = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateProductionAPIUrl;
-				deliveryUpdateUserName = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateProductionAPIUsername;
-				deliveryUpdatePassword = M4PBusinessContext.ComponentSettings.ElectroluxDeliveryUpdateProductionAPIPassword;
-			}
-
-			try
-			{
-				NetworkCredential networkCredential = new NetworkCredential(deliveryUpdateUserName, deliveryUpdatePassword);
-				HttpWebRequest request = (HttpWebRequest)WebRequest.Create(deliveryUpdateURL);
-				request.Credentials = networkCredential;
-				request.KeepAlive = false;
-				request.ContentType = "application/xml";
-				request.Method = "POST";
-				using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-				{
-					deliveryUpdateXml = CreateDeliveryUpdateRequestXml(deliveryUpdate);
-					streamWriter.Write(deliveryUpdateXml);
-				}
-
-				WebResponse response = request.GetResponse();
-
-				using (Stream electroluxDeliveryUpdateResponseStream = response.GetResponseStream())
-				{
-					using (TextReader electroluxDeliveryUpdateResponseReader = new StreamReader(electroluxDeliveryUpdateResponseStream))
-					{
-						string electroluxDeliveryUpdateResponseString = electroluxDeliveryUpdateResponseReader.ReadToEnd();
-
-						using (var stringReader = new StringReader(electroluxDeliveryUpdateResponseString))
-						{
-							deliveryUpdateResponse = GenerateDeliveryUpdateResponseFromString(electroluxDeliveryUpdateResponseString);
-						}
-					}
-				}
-			}
-			catch (Exception exp)
-			{
-				_logger.Log(exp, string.Format("Error is occuring while Sending the Delivery Update To Electrolux: Request Url is: {0}, Request body xml was {1}", deliveryUpdateURL, deliveryUpdateXml), string.Format("Electrolux delivery update for JobId: {0}", jobId), LogType.Error);
-			}
-
-			return deliveryUpdateResponse;
-		}
-		private string CreateDeliveryUpdateRequestXml(DeliveryUpdate deliveryUpdate)
-		{
-			XmlDocument xmlDoc = new XmlDocument();
-		    XmlSerializer xmlSerializer = new XmlSerializer(deliveryUpdate.GetType()); 
-			using (MemoryStream xmlStream = new MemoryStream())
-			{
-				xmlSerializer.Serialize(xmlStream, deliveryUpdate);
-				xmlStream.Position = 0;
-				xmlDoc.Load(xmlStream);
-				return string.Format(format: "{0} {1} {2}", arg0: "<ns:DeliveryUpdate xmlns:ns=\"http://esb.electrolux.com/FinalMile/Delivery\">", arg1: xmlDoc.DocumentElement.InnerXml, arg2: "</ns:DeliveryUpdate>");
-			}
-		}
-		private DeliveryUpdateResponse GenerateDeliveryUpdateResponseFromString(string updateResponseString)
-		{
-			updateResponseString = updateResponseString.Replace("NS1:DeliveryUpdateResponse", "DeliveryUpdateResponse");
-			DeliveryUpdateResponse deliveryUpdateResponse = null;
-			XmlSerializer serializer = new XmlSerializer(typeof(DeliveryUpdateResponse));
-			using (var stringReader = new StringReader(updateResponseString))
-			{
-				deliveryUpdateResponse = (DeliveryUpdateResponse)serializer.Deserialize(stringReader);
-			}
-
-			return deliveryUpdateResponse;
-		}
-
+		
 		#endregion
 	}
 }
