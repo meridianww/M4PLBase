058e55676eee55766ac7612a379398b47b66fc13
kamal.adhikary <kamal.adhikary@dreamorbit.com>
2020-04-28 16:58:13 +0530

    #48769 Job Actions and cargo Exceptions

---
+++ QA/Database/Stored Procedures/dbo.GetCargoDropDown.sql
@@ -0,0 +1,90 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+/* Copyright (2018) Meridian Worldwide Transportation Group
+   All Rights Reserved Worldwide */
+--==================================================================================    
+-- Author        : Kamal
+-- Date          : 04/27/2020     
+-- Description   : Stored Procedure to Get Combo Box For Cargo
+-- Execution Sample: EXEC [dbo].[GetCargoDropDown] 'EN',1,'Cargo','JobCargo.Id,JobCargo.CgoPartNumCode,JobCargo.CgoTitle,JobCargo.CgoSerialNumber',1,10,null,null,null,null,null,1
+-- Modified		 : 
+--=================================================================================                      
+CREATE PROCEDURE [dbo].[GetCargoDropDown]
+     @langCode NVARCHAR(10)
+	,@orgId BIGINT
+	,@entity NVARCHAR(100)
+	,@fields NVARCHAR(2000)
+	,@pageNo INT
+	,@pageSize INT
+	,@orderBy NVARCHAR(500)
+	,@like NVARCHAR(500) = NULL
+	,@where NVARCHAR(500) = NULL
+	,@primaryKeyValue NVARCHAR(100) = NULL
+	,@primaryKeyName NVARCHAR(50) = NULL
+	,@parentId BIGINT 
+AS
+BEGIN TRY
+	SET NOCOUNT ON;
+    DECLARE @sqlCommand NVARCHAR(MAX); 
+    DECLARE @newPgNo INT
+
+ SET @sqlCommand = '';
+
+	 IF(ISNULL(@primaryKeyValue, '') <> '')
+	 BEGIN
+		SET @sqlCommand += ' SELECT @newPgNo = Item FROM (SELECT ROW_NUMBER() OVER(ORDER BY ' + @entity + '.' + @primaryKeyName+') as Item  ,' +@entity + '.' + @primaryKeyName + 
+						   ' From [dbo].[JOBDL010Cargo] (NOLOCK)  ' + @entity;
+	
+		SET @sqlCommand = @sqlCommand + ' WHERE 1=1 AND '+ @entity +'.JobID = '+ CONVERT(NVARCHAR(20), @parentId) +
+		   ' (ISNULL('+ @entity +'.StatusId, 1) In (1,2)  OR ' + @primaryKeyName + '=' + @primaryKeyValue + ')';
+	    
+	
+		SET @sqlCommand += ' ) t WHERE t.' + @primaryKeyName + '=' + @primaryKeyValue;
+	
+		EXEC sp_executesql @sqlCommand, N' @newPgNo int output',@newPgNo output                                    
+		SET @newPgNo =  @newPgNo/@pageSize + 1; 
+		SET @pageSize = @newPgNo * @pageSize;
+		SET @sqlCommand=''
+	 END
+	 
+	 SET @sqlCommand += 'SELECT '+ @fields +' From [dbo].[JOBDL010Cargo] (NOLOCK) '+  @entity + ' WHERE 1=1 AND '+ @entity +'.JobID = '+ CONVERT(NVARCHAR(20), @parentId)
+
+     IF(ISNULL(@primaryKeyValue, '') <> '')
+	 BEGIN
+          SET @sqlCommand = @sqlCommand + ' AND (ISNULL('+ @entity +'.StatusId, 1) = 1  OR  '+ @entity +'.' + @primaryKeyName + '=' + @primaryKeyValue + ')';
+	 END
+	 ELSE
+	 BEGIN
+	     SET @sqlCommand = @sqlCommand + ' AND ISNULL('+ @entity +'.StatusId, 1) = 1';
+	 END	 
+
+	 IF(ISNULL(@like, '') != '')  
+	  BEGIN  
+	  SET @sqlCommand = @sqlCommand + ' AND ('  
+	   DECLARE @likeStmt NVARCHAR(MAX)  
+  
+	  SELECT @likeStmt = COALESCE(@likeStmt + ' OR ','') + Item + ' LIKE ''%' + @like + '%' + '''' FROM [dbo].[fnSplitString](@fields, ',')    
+	  SET @sqlCommand = @sqlCommand + @likeStmt + ') '  
+	  END  
+	 IF(ISNULL(@where, '') != '')  
+	  BEGIN  
+		 SET @sqlCommand = @sqlCommand + @where   
+	  END  
+		
+	SET @sqlCommand = @sqlCommand + ' ORDER BY '+ @fields +' OFFSET @pageSize * (@pageNo - 1) ROWS FETCH NEXT @PageSize ROWS ONLY OPTION (RECOMPILE);'   
+
+	EXEC sp_executesql @sqlCommand, N'@pageNo INT, @pageSize INT, @where NVARCHAR(100), @parentId BIGINT' ,  
+		 @pageNo = @pageNo,   
+		 @pageSize = @pageSize,  
+		 @where = @where,
+		 @parentId = @parentId
+
+END TRY                  
+BEGIN CATCH                  
+ DECLARE  @ErrorMessage VARCHAR(MAX) = (SELECT ERROR_MESSAGE())                  
+   ,@ErrorSeverity VARCHAR(MAX) = (SELECT ERROR_SEVERITY())                  
+   ,@RelatedTo VARCHAR(100) = (SELECT OBJECT_NAME(@@PROCID))                  
+ EXEC [dbo].[ErrorLog_InsDetails] @RelatedTo, NULL, @ErrorMessage, NULL, NULL, @ErrorSeverity    
+ END CATCH 
---
+++ QA/M4PL/M4PL.Entities/Support/CargoComboBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M4PL.Entities.Support
+{
+    public class CargoComboBox
+    {
+        public long Id { get; set; }
+        public string CgoPartNumCode { get; set; }
+        public string CgoTitle { get; set; }
+        public string CgoSerialNumber { get; set; }
+    }
+}
