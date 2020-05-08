a2e1ef15b5b67cfee59ebe7665a73969684b4135
kamal.adhikary <kamal.adhikary@dreamorbit.com>
2020-05-08 10:30:42 +0530

    #48006 Update the cancel date to current date and time when order is canceled

--- QA/M4PL/M4PL.Web/Areas/Job/Controllers/JobGatewayController.cs
+++ QA/M4PL/M4PL.Web/Areas/Job/Controllers/JobGatewayController.cs
@@ -146,11 +146,7 @@ namespace M4PL.Web.Areas.Job.Controllers
             var messages = ValidateMessages(jobGatewayView, escapeRequiredFields: escapeRequiredFields, escapeRegexField: escapeRegexField);
             if (messages != null && messages.Count() > 0 && ((messages[0] == "Code is already exist") || (messages[0] == "Code is required")))
                 messages.RemoveAt(0);
-
-            if (jobGatewayView.CurrentAction.ToLower() == "canceled" && jobGatewayView.DateCancelled == null)
-            {
-                messages.Add("Date Cancelled is Required");
-            }
+            
             if ((jobGatewayView.ProgramID == jobGatewayView.ElectroluxProgramID) && (jobGatewayView.CurrentAction.ToLower() == "exception"
                || jobGatewayView.CurrentAction.ToLower() == "reschedule"
                || jobGatewayView.CurrentAction.ToLower() == "canceled"))
@@ -998,7 +994,7 @@ namespace M4PL.Web.Areas.Job.Controllers
             FormView(strRoute);
             _formResult.Record.IsAction = true;
             _formResult.Record.GwyCompleted = true;
-            _formResult.Record.GwyGatewayACD = DateTime.UtcNow;
+            _formResult.Record.GwyGatewayACD = DateTime.UtcNow;           
 
 
             if (route.Filters != null)
@@ -1008,6 +1004,9 @@ namespace M4PL.Web.Areas.Job.Controllers
                 _formResult.Record.StatusCode = route.Filters.Value.Substring(route.Filters.Value.LastIndexOf('-') + 1);
                 _formResult.Record.GwyShipApptmtReasonCode = _formResult.Record.StatusCode;
             }
+            if (_formResult.Record.GwyGatewayCode.ToLower() == "canceled")
+                _formResult.Record.DateCancelled = DateTime.UtcNow;
+
             var result = _jobGatewayCommands.JobActionCodeByTitle(route.ParentRecordId, _formResult.Record.GwyTitle);
             _formResult.Record.GwyShipApptmtReasonCode = result.PgdShipApptmtReasonCode;
             _formResult.Record.GwyShipStatusReasonCode = result.PgdShipStatusReasonCode;
