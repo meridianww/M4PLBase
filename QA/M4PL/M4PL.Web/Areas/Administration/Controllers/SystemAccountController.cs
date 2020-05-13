55e84ba33d6eb52e0a92c164daa327af0e904b7a
kamal.adhikary <kamal.adhikary@dreamorbit.com>
2020-05-13 19:50:34 +0530

    #49106,#49105

--- QA/M4PL/M4PL.Web/Areas/Administration/Controllers/SystemAccountController.cs
+++ QA/M4PL/M4PL.Web/Areas/Administration/Controllers/SystemAccountController.cs
@@ -24,6 +24,7 @@ namespace M4PL.Web.Areas.Administration.Controllers
 {
     public class SystemAccountController : BaseController<SystemAccountView>
     {
+        private readonly ISystemAccountCommands _systemAccountCommands;
         /// <summary>
         /// Interacts with the interfaces to get the System Account(OpnSezMe) user's details of individual page and renders to the respective page
         /// Gets the page related information on the cache basis
@@ -34,14 +35,24 @@ namespace M4PL.Web.Areas.Administration.Controllers
             : base(systemAccountCommands)
         {
             _commonCommands = commonCommands;
+            _systemAccountCommands = systemAccountCommands;
         }
         [HttpPost, ValidateInput(false)]
         public PartialViewResult DataViewBatchUpdate(MVCxGridViewBatchUpdateValues<SystemAccountView, long> systemAccountView, string strRoute, string gridName)
         {
-
             var route = Newtonsoft.Json.JsonConvert.DeserializeObject<Entities.Support.MvcRoute>(strRoute);
-            systemAccountView.Insert.ForEach(c => { c.OrganizationId = SessionProvider.ActiveUser.OrganizationId; });
-            systemAccountView.Update.ForEach(c => { c.OrganizationId = SessionProvider.ActiveUser.OrganizationId; });
+            systemAccountView.Insert.ForEach(c =>
+            {
+                c.OrganizationId = SessionProvider.ActiveUser.OrganizationId;
+            });
+            systemAccountView.Update.ForEach(c =>
+            {
+                c.OrganizationId = SessionProvider.ActiveUser.OrganizationId;
+            });
+            for (int i = 0; i < systemAccountView.Update.Count(); i++)
+            {
+                systemAccountView.Update[i].SysPassword = _systemAccountCommands.Get(systemAccountView.Update[i].Id).SysPassword;
+            }
             var batchError = BatchUpdate(systemAccountView, route, gridName);
             if (!batchError.Any(b => b.Key == -100))//100 represent model state so no need to show message
             {
@@ -53,65 +64,65 @@ namespace M4PL.Web.Areas.Administration.Controllers
 
             SetGridResult(route);
             return ProcessCustomBinding(route, MvcConstants.ActionDataView);
-        }        
+        }
         public PartialViewResult DataViewBatchUpdateOnSysAdminChange(string input, string strRoute, string gridName)
         {
             var systemAccountView = JsonConvert.DeserializeObject<MVCxGridViewBatchUpdateValues<SystemAccountView, long>>(input); ;
             var route = Newtonsoft.Json.JsonConvert.DeserializeObject<Entities.Support.MvcRoute>(strRoute);
             systemAccountView.Insert.ForEach(c => { c.OrganizationId = SessionProvider.ActiveUser.OrganizationId; });
             systemAccountView.Update.ForEach(c => { c.OrganizationId = SessionProvider.ActiveUser.OrganizationId; });
-            
-                var batchError = BatchUpdate(systemAccountView, route, gridName);
-                if (!batchError.Any(b => b.Key == -100))//100 represent model state so no need to show message
-                {
-                    var displayMessage = batchError.Count == 0 ? _commonCommands.GetDisplayMessageByCode(MessageTypeEnum.Success, DbConstants.UpdateSuccess) : _commonCommands.GetDisplayMessageByCode(MessageTypeEnum.Error, DbConstants.UpdateError);
 
-                    displayMessage.Operations.ToList().ForEach(op => op.SetupOperationRoute(route));
-                    ViewData[WebApplicationConstants.GridBatchEditDisplayMessage] = displayMessage;
-                }
+            var batchError = BatchUpdate(systemAccountView, route, gridName);
+            if (!batchError.Any(b => b.Key == -100))//100 represent model state so no need to show message
+            {
+                var displayMessage = batchError.Count == 0 ? _commonCommands.GetDisplayMessageByCode(MessageTypeEnum.Success, DbConstants.UpdateSuccess) : _commonCommands.GetDisplayMessageByCode(MessageTypeEnum.Error, DbConstants.UpdateError);
+
+                displayMessage.Operations.ToList().ForEach(op => op.SetupOperationRoute(route));
+                ViewData[WebApplicationConstants.GridBatchEditDisplayMessage] = displayMessage;
+            }
             SetGridResult(route);
             return ProcessCustomBinding(route, MvcConstants.ActionDataView);
         }
         #region RichEdit
-		public ActionResult RichEditComments(string strRoute, M4PL.Entities.Support.Filter docId)
-		{
-			long newDocumentId;
-			var route = JsonConvert.DeserializeObject<MvcRoute>(strRoute);
-			var byteArray = route.GetVarbinaryByteArray(ByteArrayFields.SysComments.ToString());
-			if (docId != null && docId.FieldName.Equals("ArbRecordId") && long.TryParse(docId.Value, out newDocumentId))
-			{
-				byteArray = route.GetVarbinaryByteArray(newDocumentId, ByteArrayFields.SysComments.ToString());
-			}
-			if (route.RecordId > 0)
-				byteArray.Bytes = _commonCommands.GetByteArrayByIdAndEntity(byteArray)?.Bytes;
-			return base.RichEditFormView(byteArray);
-		}
-		#endregion RichEdit
-		public override ActionResult AddOrEdit(SystemAccountView systemAccountView)
+        public ActionResult RichEditComments(string strRoute, M4PL.Entities.Support.Filter docId)
+        {
+            long newDocumentId;
+            var route = JsonConvert.DeserializeObject<MvcRoute>(strRoute);
+            var byteArray = route.GetVarbinaryByteArray(ByteArrayFields.SysComments.ToString());
+            if (docId != null && docId.FieldName.Equals("ArbRecordId") && long.TryParse(docId.Value, out newDocumentId))
+            {
+                byteArray = route.GetVarbinaryByteArray(newDocumentId, ByteArrayFields.SysComments.ToString());
+            }
+            if (route.RecordId > 0)
+                byteArray.Bytes = _commonCommands.GetByteArrayByIdAndEntity(byteArray)?.Bytes;
+            return base.RichEditFormView(byteArray);
+        }
+        #endregion RichEdit
+        public override ActionResult AddOrEdit(SystemAccountView systemAccountView)
         {
 
             var route = new MvcRoute(BaseRoute, MvcConstants.ActionDataView);
-           
+
             var commentsByteArray = systemAccountView.ArbRecordId.GetVarbinaryByteArray(EntitiesAlias.SystemAccount, ByteArrayFields.SysComments.ToString());
             var byteArray = new List<ByteArray> {
                 commentsByteArray
             };
             systemAccountView.IsFormView = true;
-            
+
             SessionProvider.ActiveUser.SetRecordDefaults(systemAccountView, Request.Params[WebApplicationConstants.UserDateTime]);
             systemAccountView.SysOrgId = SessionProvider.ActiveUser.OrganizationId;
 
             var messages = ValidateMessages(systemAccountView);
-           
+
 
             if (systemAccountView.SysOrgRefRoleId.HasValue && _commonCommands.GetRefRoleSecurities(new ActiveUser { UserId = SessionProvider.ActiveUser.UserId, RoleId = systemAccountView.SysOrgRefRoleId.GetValueOrDefault() }).Count == 0)
                 messages.Add(_commonCommands.GetDisplayMessageByCode(MessageTypeEnum.Warning, DbConstants.NoSecuredModule).Description);
 
             if (messages.Any())
                 return Json(new { status = false, byteArray = byteArray, errMessages = messages }, JsonRequestBehavior.AllowGet);
-            
+
             var record = systemAccountView.Id > 0 ? UpdateForm(systemAccountView) : SaveForm(systemAccountView);
-           
+
             if (record is SysRefModel)
             {
                 route.RecordId = record.Id;
@@ -138,10 +149,10 @@ namespace M4PL.Web.Areas.Administration.Controllers
             string inputNo = JsonConvert.SerializeObject(systemAccountView);
             var noOperation = displayMessage.Operations.FirstOrDefault(x => x.SysRefName.Equals(MessageOperationTypeEnum.No.ToString()));
             noOperation.ClickEvent = "function(s, e) {{ " + string.Format(JsConstants.SaveChangesOnIsAdminChange, inputNo, strRoute) + " }}";
-            return Json(new { status = false, displayMessage = displayMessage, route = route ,byteArray=byteArray}, JsonRequestBehavior.AllowGet);
-            
+            return Json(new { status = false, displayMessage = displayMessage, route = route, byteArray = byteArray }, JsonRequestBehavior.AllowGet);
+
         }
-       
+
         public override ActionResult FormView(string strRoute)
         {
             base.FormView(strRoute);
@@ -165,5 +176,6 @@ namespace M4PL.Web.Areas.Administration.Controllers
                 }
             });
             return PartialView(_gridResult);
-        }    }
+        }
+    }
 }
