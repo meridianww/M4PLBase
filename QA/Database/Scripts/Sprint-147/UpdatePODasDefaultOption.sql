350725f8c46d91e5adda73a971265ecd8aedae93
manoj.kumar <manoj.kumar@DOLAP122>
2020-06-12 12:10:11 +0530

    Updated the changes for TimeZone and Caption for Doc Type

--- QA/Database/Scripts/Sprint-147/UpdatePODasDefaultOption.sql
+++ QA/Database/Scripts/Sprint-147/UpdatePODasDefaultOption.sql
@@ -1,5 +1,5 @@
 UPDATE SYSTM000Ref_Options SET SysDefault = 0 Where SysLookupCode='JobDocReferenceType'
 UPDATE SYSTM000Ref_Options SET SysDefault = 1 Where SysLookupCode='JobDocReferenceType' AND SysOptionName = 'POD'
 
-UPDATE [dbo].[SYSTM000ColumnsAlias] SET ColAliasName = 'Document Type', ColCaption = 'Document Type', 
+UPDATE [dbo].[SYSTM000ColumnsAlias] SET ColAliasName = 'Document Type', ColCaption = 'Doc Type', 
 ColGridAliasName = 'Document Type' where ColTableName = 'JobDocReference' AND ColColumnName = 'DocTypeId'
