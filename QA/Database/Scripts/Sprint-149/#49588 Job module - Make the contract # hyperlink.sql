e7bd6d411770f9103e79a6c9dc041f848b8830c9
kamal.adhikary <kamal.adhikary@dreamorbit.com>
2020-06-18 04:16:35 +0530

    #49587 Delete and Job id column should be available only to system admin

--- QA/Database/Scripts/Sprint-149/#49588 Job module - Make the contract # hyperlink.sql
+++ QA/Database/Scripts/Sprint-149/#49588 Job module - Make the contract # hyperlink.sql
@@ -1,15 +1,15 @@
 
-UPDATE SYSTM000ColumnsAlias SET ColIsVisible=0 WHERE ColTableName = 'JobCard' AND ColColumnName = 'Id'
+UPDATE SYSTM000ColumnsAlias SET ColIsVisible=1 WHERE ColTableName = 'JobCard' AND ColColumnName = 'Id'
 UPDATE SYSTM000ColumnSettingsByUser set ColNotVisible = 
-'Id,JobSiteCode,PlantIDCode,JobTotalWeight,JobTotalCubes,JobOriginSiteName,JobDeliveryDateTimeActual,JobOriginDateTimeActual'
-WHERE ColTableName = 'JobCard'
+'JobSiteCode,PlantIDCode,JobTotalWeight,JobTotalCubes,JobOriginSiteName,JobDeliveryDateTimeActual,JobOriginDateTimeActual'
+WHERE ColTableName = 'JobCard' AND ColUserId = -1
 
-UPDATE SYSTM000ColumnsAlias SET ColIsVisible=0 WHERE ColTableName = 'JobAdvanceReport' AND ColColumnName = 'Id'
+UPDATE SYSTM000ColumnsAlias SET ColIsVisible=1 WHERE ColTableName = 'JobAdvanceReport' AND ColColumnName = 'Id'
 UPDATE SYSTM000ColumnSettingsByUser set ColNotVisible = 
-'Id,JobTotalWeight,CgoPartCode,JobBOL,CustTitle,JobManifestNo,PlantIDCode,JobSiteCode'
+'JobTotalWeight,CgoPartCode,JobBOL,CustTitle,JobManifestNo,PlantIDCode,JobSiteCode'
 WHERE ColTableName = 'JobAdvanceReport' AND ColUserId = -1
 
-UPDATE SYSTM000ColumnsAlias SET ColIsVisible=0 WHERE ColTableName = 'Job' AND ColColumnName = 'Id'
+UPDATE SYSTM000ColumnsAlias SET ColIsVisible=1 WHERE ColTableName = 'Job' AND ColColumnName = 'Id'
 UPDATE SYSTM000ColumnSettingsByUser set ColNotVisible = 
-'Id,JobSiteCode,PlantIDCode,JobTotalWeight,JobTotalCubes,JobOriginSiteName,JobDeliveryDateTimeActual,JobOriginDateTimeActual'
+'JobSiteCode,PlantIDCode,JobTotalWeight,JobTotalCubes,JobOriginSiteName,JobDeliveryDateTimeActual,JobOriginDateTimeActual'
 WHERE ColTableName = 'Job' AND ColUserId = -1
