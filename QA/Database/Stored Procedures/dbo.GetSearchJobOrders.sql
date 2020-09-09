7920f0cd18b463f38901a2370e20e010e4fd4411
kamal.adhikary <kamal.adhikary@DOLAP400>
2020-09-09 14:33:01 +0530

    Mobile app changes

--- QA/Database/Stored Procedures/dbo.GetSearchJobOrders.sql
+++ QA/Database/Stored Procedures/dbo.GetSearchJobOrders.sql
@@ -25,40 +25,107 @@ BEGIN
 			)
 	BEGIN
 		SELECT JOB.Id
-		,JOB.JobCustomerSalesOrder AS CustomerSalesOrder
-		,JOB.JobGatewayStatus AS GatewayStatus
-		,JOB.JobDeliveryDateTimePlanned AS DeliveryDatePlanned
-		,JOB.JobOriginDateTimePlanned AS ArrivalDatePlanned
-	FROM JOBDL000Master JOB WHERE JOB.JobCustomerSalesOrder = @search
+			,JOB.JobCustomerSalesOrder AS CustomerSalesOrder
+			,JOB.JobGatewayStatus AS GatewayStatus
+			,JOB.JobDeliveryDateTimePlanned AS DeliveryDatePlanned
+			,JOB.JobOriginDateTimePlanned AS ArrivalDatePlanned
+		FROM JOBDL000Master JOB
+		WHERE JOB.JobCustomerSalesOrder = @search
 	END
 	ELSE
 	BEGIN
-		 SELECT JOB.Id
-		,JOB.JobCustomerSalesOrder AS CustomerSalesOrder
-		,JOB.JobGatewayStatus AS GatewayStatus
-		,JOB.JobDeliveryDateTimePlanned AS DeliveryDatePlanned
-		,JOB.JobOriginDateTimePlanned AS ArrivalDatePlanned
-	FROM  CUST000Master CUST
-	INNER JOIN PRGRM000Master PRG ON PRG.PrgCustID = CUST.Id AND CUST.StatusId=1
-	INNER JOIN JOBDL000Master JOB ON JOB.ProgramID = PRG.ID AND PRG.StatusId =1 AND JOB.StatusId=1
-	WHERE( CUST.CustTitle LIKE '%' + @search + '%'
-			OR JOB.JobCustomerSalesOrder LIKE '%' + @search + '%'
-			OR JobBOL LIKE '%' + @search + '%'
-			OR JobBOLMaster LIKE '%' + @search + '%'
-			OR JobManifestNo LIKE '%' + @search + '%'
-			OR JobGatewayStatus LIKE '%' + @search + '%'
-			OR JobSiteCode LIKE '%' + @search + '%'
-			OR JobDeliveryStreetAddress LIKE '%' + @search + '%'
-			OR JobDeliveryStreetAddress2 LIKE '%' + @search + '%'
-			OR JobDeliveryStreetAddress3 LIKE '%' + @search + '%'
-			OR JobDeliveryCity LIKE '%' + @search + '%'
-			OR JobDeliveryState LIKE '%' + @search + '%'
-			OR JobDeliveryPostalCode LIKE '%' + @search + '%'
-			OR JobCarrierContract LIKE '%' + @search + '%'
-			OR JobDeliverySitePOC LIKE '%' + @search + '%'
-			OR JobDeliverySiteName LIKE '%' + @search + '%'
-			OR PlantIDCode LIKE '%' + @search + '%'
-			OR JobSellerSiteName LIKE '%' + @search + '%'
-			)
+		DECLARE @IsJobAdmin BIT = 0
+
+		IF OBJECT_ID('tempdb..#EntityIdTemp') IS NOT NULL
+		BEGIN
+			DROP TABLE #EntityIdTemp
+		END
+
+		CREATE TABLE #EntityIdTemp (EntityId BIGINT)
+
+		INSERT INTO #EntityIdTemp
+		EXEC [dbo].[GetCustomEntityIdByEntityName] @userId
+			,@roleId
+			,@orgId
+			,'JOB'
+
+		IF EXISTS (
+				SELECT 1
+				FROM #EntityIdTemp
+				WHERE ISNULL(EntityId, 0) = - 1
+				)
+		BEGIN
+			SET @IsJobAdmin = 1
+		END
+
+		IF (@IsJobAdmin = 0)
+		BEGIN
+			SELECT JOB.Id
+				,JOB.JobCustomerSalesOrder AS CustomerSalesOrder
+				,JOB.JobGatewayStatus AS GatewayStatus
+				,JOB.JobDeliveryDateTimePlanned AS DeliveryDatePlanned
+				,JOB.JobOriginDateTimePlanned AS ArrivalDatePlanned
+			FROM CUST000Master CUST
+			INNER JOIN PRGRM000Master PRG ON PRG.PrgCustID = CUST.Id
+				AND CUST.StatusId = 1
+			INNER JOIN JOBDL000Master JOB ON JOB.ProgramID = PRG.ID
+				AND PRG.StatusId = 1
+				AND JOB.StatusId = 1
+			INNER JOIN #EntityIdTemp tmp ON tmp.[EntityId] = JOB.[Id]
+			WHERE (
+					CUST.CustTitle LIKE '%' + @search + '%'
+					OR JOB.JobCustomerSalesOrder LIKE '%' + @search + '%'
+					OR JobBOL LIKE '%' + @search + '%'
+					OR JobBOLMaster LIKE '%' + @search + '%'
+					OR JobManifestNo LIKE '%' + @search + '%'
+					OR JobGatewayStatus LIKE '%' + @search + '%'
+					OR JobSiteCode LIKE '%' + @search + '%'
+					OR JobDeliveryStreetAddress LIKE '%' + @search + '%'
+					OR JobDeliveryStreetAddress2 LIKE '%' + @search + '%'
+					OR JobDeliveryStreetAddress3 LIKE '%' + @search + '%'
+					OR JobDeliveryCity LIKE '%' + @search + '%'
+					OR JobDeliveryState LIKE '%' + @search + '%'
+					OR JobDeliveryPostalCode LIKE '%' + @search + '%'
+					OR JobCarrierContract LIKE '%' + @search + '%'
+					OR JobDeliverySitePOC LIKE '%' + @search + '%'
+					OR JobDeliverySiteName LIKE '%' + @search + '%'
+					OR PlantIDCode LIKE '%' + @search + '%'
+					OR JobSellerSiteName LIKE '%' + @search + '%'
+					)
+		END
+		ELSE
+		BEGIN
+			SELECT JOB.Id
+				,JOB.JobCustomerSalesOrder AS CustomerSalesOrder
+				,JOB.JobGatewayStatus AS GatewayStatus
+				,JOB.JobDeliveryDateTimePlanned AS DeliveryDatePlanned
+				,JOB.JobOriginDateTimePlanned AS ArrivalDatePlanned
+			FROM CUST000Master CUST
+			INNER JOIN PRGRM000Master PRG ON PRG.PrgCustID = CUST.Id
+				AND CUST.StatusId = 1
+			INNER JOIN JOBDL000Master JOB ON JOB.ProgramID = PRG.ID
+				AND PRG.StatusId = 1
+				AND JOB.StatusId = 1
+			WHERE (
+					CUST.CustTitle LIKE '%' + @search + '%'
+					OR JOB.JobCustomerSalesOrder LIKE '%' + @search + '%'
+					OR JobBOL LIKE '%' + @search + '%'
+					OR JobBOLMaster LIKE '%' + @search + '%'
+					OR JobManifestNo LIKE '%' + @search + '%'
+					OR JobGatewayStatus LIKE '%' + @search + '%'
+					OR JobSiteCode LIKE '%' + @search + '%'
+					OR JobDeliveryStreetAddress LIKE '%' + @search + '%'
+					OR JobDeliveryStreetAddress2 LIKE '%' + @search + '%'
+					OR JobDeliveryStreetAddress3 LIKE '%' + @search + '%'
+					OR JobDeliveryCity LIKE '%' + @search + '%'
+					OR JobDeliveryState LIKE '%' + @search + '%'
+					OR JobDeliveryPostalCode LIKE '%' + @search + '%'
+					OR JobCarrierContract LIKE '%' + @search + '%'
+					OR JobDeliverySitePOC LIKE '%' + @search + '%'
+					OR JobDeliverySiteName LIKE '%' + @search + '%'
+					OR PlantIDCode LIKE '%' + @search + '%'
+					OR JobSellerSiteName LIKE '%' + @search + '%'
+					)
+		END
 	END
 END
