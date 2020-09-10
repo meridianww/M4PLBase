cbbcd1bb972c468a19395cd0d98d1682be4cee37
Prashant Aggarwal <prashant.aggarwal@dreamorbit.com>
2020-09-10 09:36:56 +0530

    Commit Changes

--- QA/Database/Scripts/Sprint 159/cancel insert into sys option table.sql
+++ QA/Database/Scripts/Sprint 159/cancel insert into sys option table.sql
@@ -1,7 +1,7 @@
 DECLARE @MaxCountID INT
-IF NOT EXISTS(SELECT 1 FROM SYSTM000Ref_Options WHERE SysLookupCode='Status' AND SysOptionName ='Cancel')
+IF NOT EXISTS(SELECT 1 FROM SYSTM000Ref_Options WHERE SysLookupCode='Status' AND SysOptionName ='Canceled')
 BEGIN
     SELECT @MaxCountID = MAX(ID) FROM SYSTM000Ref_Options WHERE SysLookupCode='Status'
 	INSERT INTO dbo.SYSTM000Ref_Options (SysLookupId, SysLookupCode, SysOptionName, SysSortOrder, SysDefault, IsSysAdmin, StatusId, DateEntered, EnteredBy, DateChanged, ChangedBy)
-	VALUES (39, 'Status', 'Cancel', @MaxCountID, 0, 0, 1, '2017-05-04 12:50:39.99', NULL, '2019-12-05 10:54:42.613', 'nfujimoto')
+	VALUES (39, 'Status', 'Canceled', @MaxCountID, 0, 0, 1, '2017-05-04 12:50:39.99', NULL, '2019-12-05 10:54:42.613', 'nfujimoto')
 END
