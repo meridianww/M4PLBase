4e1112d4764da97576ddde9bc511da0542facc5e
kamal.adhikary <kamal.adhikary@DOLAP400>
2020-09-28 17:18:50 +0530

    no result show in advance report in first time

--- QA/M4PL/M4PL.Web/Areas/BaseController.cs
+++ QA/M4PL/M4PL.Web/Areas/BaseController.cs
@@ -111,7 +111,11 @@ namespace M4PL.Web.Areas
                 || currentPagedDataInfo.WhereCondition.IndexOf("StatusId") == -1) && !route.IsJobParentEntityUpdated)
                 currentPagedDataInfo.WhereCondition = string.Format("{0} AND {1}.{2} = {3}", currentPagedDataInfo.WhereCondition, route.Entity, "StatusId", 1);
 
-            if (route.Entity == EntitiesAlias.JobHistory)
+            if (route.Entity == EntitiesAlias.JobAdvanceReport && reportTypeId == 0)
+            {
+                _gridResult.Records = new List<TView>();
+            }
+            else if (route.Entity == EntitiesAlias.JobHistory)
             {
                 route.IsPBSReport = false;
                 currentPagedDataInfo.RecordId = route.RecordId;
