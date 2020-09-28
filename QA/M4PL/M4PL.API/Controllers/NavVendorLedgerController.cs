fb6391e87f89789c50b04cf792a2f9dbae39e298
Prashant Aggarwal <prashant.aggarwal@dreamorbit.com>
2020-09-28 15:36:10 +0530

    Commit Changes

---
+++ QA/M4PL/M4PL.API/Controllers/NavVendorLedgerController.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright
+/******************************************************************************
+* Copyright (C) 2016-2020 Meridian Worldwide Transportation Group - All Rights Reserved.
+*
+* Proprietary and confidential. Unauthorized copying of this file, via any
+* medium is strictly prohibited without the explicit permission of Meridian Worldwide Transportation Group.
+******************************************************************************/
+#endregion Copyright
+
+using M4PL.API.Filters;
+using System.Collections.Generic;
+using System.Web.Http;
+using M4PL.Business.Finance.VendorLedger;
+using M4PL.Entities.Finance.VendorLedger;
+
+namespace M4PL.API.Controllers
+{
+	[CustomAuthorize]
+	[RoutePrefix("api/NavVendorLedger")]
+	public class NavVendorLedgerController : ApiController
+	{
+		private readonly INavVendorLedgerCommands _navVendorLedgerCommands;
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="NavVendorLedgerController"/> class.
+		/// </summary>
+		public NavVendorLedgerController(INavVendorLedgerCommands navVendorLedgerCommands)
+		{
+			_navVendorLedgerCommands = navVendorLedgerCommands;
+		}
+
+		[HttpGet]
+		public virtual List<VendorCheckedInvoice> GetVendorCheckedInvoice(string postedInvoiceNumber)
+		{
+			_navVendorLedgerCommands.ActiveUser = Models.ApiContext.ActiveUser;
+			return _navVendorLedgerCommands.GetVendorCheckedInvoice(postedInvoiceNumber);
+		}
+	}
+}
