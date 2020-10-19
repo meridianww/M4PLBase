6d4ffdb373568eea8fe6f6534ccfc4eced5a5a4a
kamal.adhikary <kamal.adhikary@DOLAP400>
2020-10-19 10:36:57 +0530

    Order type and shipment type added into procedure

--- QA/Database/Stored Procedures/dbo.GetMultiJobActions.sql
+++ QA/Database/Stored Procedures/dbo.GetMultiJobActions.sql
@@ -3,14 +3,14 @@
 -- Author:                    Kamal         
 -- Create date:               13/10/2020     
 -- Description:               Get all job Actions
--- Execution:                 EXEC [dbo].[GetMultiJobActions] '154728'
+-- Execution:                 EXEC [dbo].[GetMultiJobActions] '154728,3047'
 -- Modified on:  
 -- Modified Desc:  
 -- =============================================
-CREATE PROCEDURE [dbo].[GetMultiJobActions] @jobIds NVARCHAR(MAX)
+ALTER PROCEDURE [dbo].[GetMultiJobActions] @jobIds NVARCHAR(MAX)
 AS
 BEGIN
-    DECLARE @JobTypeCount INT =0, @ShipmentTypeCount INT =0;
+    DECLARE @JobTypeCount INT =0, @ShipmentTypeCount INT =0,@JobType NVARCHAR(20) = NULL, @ShipmentType NVARCHAR(20) = NULL;
     DECLARE @TempJobIds AS TABLE (JobId BIGINT)
 	INSERT INTO @TempJobIds
 		SELECT Item
@@ -21,6 +21,9 @@ BEGIN
 	IF(	@JobTypeCount = 1 AND @ShipmentTypeCount = 1)
 	BEGIN
 	    DECLARE @IsSchedule BIT = NULL,@IsScheduleCount INT =0;
+		SELECT DISTINCT @JobType = JobType, @ShipmentType= ShipmentType FROM JOBDL000Master JOB 
+		INNER JOIN @TempJobIds TMP ON TMP.JobId = JOB.Id
+
 		SELECT @IsScheduleCount=COUNT(DISTINCT JobIsSchedule) FROM JOBDL000Master JOB
 		INNER JOIN @TempJobIds TMP ON TMP.JobId = JOB.Id
 		IF(@IsScheduleCount =1)
@@ -34,6 +37,7 @@ BEGIN
 			INNER JOIN JOBDL000Master JOB ON JOB.ProgramID = GATEWAY.PgdProgramID 
 			INNER JOIN @TempJobIds TMP ON TMP.JobId = JOB.Id
 			WHERE GATEWAY.GatewayTypeId = 86 AND GATEWAY.StatusId IN (1,2)
+			AND GATEWAY.PgdShipmentType = @ShipmentType AND GATEWAY.PgdOrderType = @JobType
 			AND GATEWAY.PgdGatewayCode NOT IN (
 							'Schedule-NS'
 							,'Schedule'
@@ -49,6 +53,7 @@ BEGIN
 			INNER JOIN JOBDL000Master JOB ON JOB.ProgramID = GATEWAY.PgdProgramID 
 			INNER JOIN @TempJobIds TMP ON TMP.JobId = JOB.Id
 			WHERE GATEWAY.GatewayTypeId = 86 AND GATEWAY.StatusId IN (1,2)
+			AND GATEWAY.PgdShipmentType = @ShipmentType AND GATEWAY.PgdOrderType = @JobType
 			AND GATEWAY.PgdGatewayCode <> 'Delivery Window'
 			AND GATEWAY.PgdGatewayCode NOT IN (
 				SELECT PgdGatewayCode
@@ -61,6 +66,7 @@ BEGIN
 			INNER JOIN JOBDL000Master JOB ON JOB.ProgramID = GATEWAY.PgdProgramID 
 			INNER JOIN @TempJobIds TMP ON TMP.JobId = JOB.Id
 			WHERE GATEWAY.GatewayTypeId = 86 AND GATEWAY.StatusId IN (1,2)
+			AND GATEWAY.PgdShipmentType = @ShipmentType AND GATEWAY.PgdOrderType = @JobType
 			AND GATEWAY.PgdGatewayCode NOT IN (
 							'Schedule-NS'
 							,'Schedule'
