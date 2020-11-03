6cdaf372d903bab2c98a5df458933d3bc5006868
Prashant Aggarwal <prashant.aggarwal@dreamorbit.com>
2020-11-03 13:17:35 +0530

    Email Alerts - IT Issues #357

--- QA/Database/Stored Procedures/dbo.GetXcblExceptionInfo.sql
+++ QA/Database/Stored Procedures/dbo.GetXcblExceptionInfo.sql
@@ -23,6 +23,33 @@ BEGIN
 		WHERE TranMessageCode <> 'Success'
 			AND TranWebMessageNumber = '03.02'
 	END
+	IF (@scenarioTypeId = 6)
+	BEGIN
+		SELECT TranDateTime TransactionDatetime
+			,TranOrderNo OrderNumber
+			,TranWebDocumentID DocumentId
+		FROM [dbo].[XCBL_MER010TransactionLog]
+		WHERE TranMessageCode <> 'Success'
+			AND TranWebMessageNumber = '03.01'
+	END
+	IF (@scenarioTypeId = 7)
+	BEGIN
+		SELECT TranDateTime TransactionDatetime
+			,TranOrderNo OrderNumber
+			,TranWebDocumentID DocumentId
+		FROM [dbo].[XCBL_MER010TransactionLog]
+		WHERE TranMessageCode <> 'Success'
+			AND TranWebMessageNumber IN ('06.04','06.07','06.08','03.08')
+	END
+	IF (@scenarioTypeId = 8)
+	BEGIN
+		SELECT TranDateTime TransactionDatetime
+			,TranOrderNo OrderNumber
+			,TranWebDocumentID DocumentId
+		FROM [dbo].[XCBL_MER010TransactionLog]
+		WHERE TranMessageCode <> 'Success'
+			AND TranWebMessageNumber IN ('03.08', '03.12')
+	END
 END
 GO
 
