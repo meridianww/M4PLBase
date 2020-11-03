3973c6a689ce04c282e4bfd1c268536e7bbd75df
Prashant Aggarwal <prashant.aggarwal@dreamorbit.com>
2020-11-03 16:53:15 +0530

    Email Alerts - IT Issues #357

--- QA/Database/Stored Procedures/dbo.GetXcblExceptionInfo.sql
+++ QA/Database/Stored Procedures/dbo.GetXcblExceptionInfo.sql
@@ -9,10 +9,16 @@ GO
 -- Create date: 11/03/2020
 -- Description:	Get xCBL Exception Details
 -- =============================================
-CREATE PROCEDURE [dbo].[GetXcblExceptionInfo]  (@scenarioTypeId INT)
+CREATE PROCEDURE [dbo].[GetXcblExceptionInfo] (@scenarioTypeId INT)
 AS
 BEGIN
 	SET NOCOUNT ON;
+
+	--DECLARE @LastExecutionDate DATETIME2(7)
+
+	--SELECT @LastExecutionDate = ExecutionDateTime
+	--FROM [dbo].[EmailProcessingLog]
+	--WHERE [ScenarioTypeId] = @scenarioTypeId
 
 	IF (@scenarioTypeId = 5)
 	BEGIN
@@ -22,7 +28,13 @@ BEGIN
 		FROM [dbo].[XCBL_MER010TransactionLog]
 		WHERE TranMessageCode <> 'Success'
 			AND TranWebMessageNumber = '03.02'
+			--AND TranDateTime > CASE 
+			--	WHEN @LastExecutionDate IS NULL
+			--		THEN dateadd(HOUR, - 4, getdate())
+			--	ELSE @LastExecutionDate
+			--	END
 	END
+
 	IF (@scenarioTypeId = 6)
 	BEGIN
 		SELECT TranDateTime TransactionDatetime
@@ -31,7 +43,13 @@ BEGIN
 		FROM [dbo].[XCBL_MER010TransactionLog]
 		WHERE TranMessageCode <> 'Success'
 			AND TranWebMessageNumber = '03.01'
+			--AND TranDateTime > CASE 
+			--	WHEN @LastExecutionDate IS NULL
+			--		THEN dateadd(HOUR, - 4, getdate())
+			--	ELSE @LastExecutionDate
+			--	END
 	END
+
 	IF (@scenarioTypeId = 7)
 	BEGIN
 		SELECT TranDateTime TransactionDatetime
@@ -39,8 +57,14 @@ BEGIN
 			,TranWebDocumentID DocumentId
 		FROM [dbo].[XCBL_MER010TransactionLog]
 		WHERE TranMessageCode <> 'Success'
-			AND TranWebMessageNumber IN ('06.04','06.07','06.08','03.08')
+			AND TranWebMessageNumber IN ('06.04', '06.07', '06.08', '03.08')
+			--AND TranDateTime > CASE 
+			--	WHEN @LastExecutionDate IS NULL
+			--		THEN dateadd(HOUR, - 4, getdate())
+			--	ELSE @LastExecutionDate
+			--	END
 	END
+
 	IF (@scenarioTypeId = 8)
 	BEGIN
 		SELECT TranDateTime TransactionDatetime
@@ -49,6 +73,11 @@ BEGIN
 		FROM [dbo].[XCBL_MER010TransactionLog]
 		WHERE TranMessageCode <> 'Success'
 			AND TranWebMessageNumber IN ('03.08', '03.12')
+			--AND TranDateTime > CASE 
+			--	WHEN @LastExecutionDate IS NULL
+			--		THEN dateadd(HOUR, - 4, getdate())
+			--	ELSE @LastExecutionDate
+			--	END
 	END
 END
 GO
