4491a1d3ac353ab001fc45494568ab626e7fc24b
prashant.dewangan <prashant.dewangan@dreamorbit.com>
2020-11-09 12:44:59 +0530

    51269: Price Charge and Cost Charge Report fixes And SP fine tuning and Email Service controller documentation

--- QA/M4PL/M4PL.API/Controllers/EmailServiceController.cs
+++ QA/M4PL/M4PL.API/Controllers/EmailServiceController.cs
@@ -29,6 +29,9 @@ using System.Web.Http.Description;
 
 namespace M4PL.API.Controllers
 {
+	/// <summary>
+	/// Endpoints to serve Email Service (IT Email alerts)
+	/// </summary>
 	[CustomAuthorize]
 	[RoutePrefix("api/EmailService")]
 	public class EmailServiceController : ApiController
@@ -55,6 +58,13 @@ namespace M4PL.API.Controllers
 			return _emailCommands.GetSMTPEmailDetail(emailCount, toHours, fromHours);
 		}
 
+		/// <summary>
+		/// Updates Email status and Retry Attempts on EmailDetail for the supplied id
+		/// </summary>
+		/// <param name="id">Supplied Id of EmailDetail</param>
+		/// <param name="emailStatus">Email Status</param>
+		/// <param name="retryAttampts">Retry Attempts</param>
+		/// <returns></returns>
 		[HttpGet]
 		[Route("UpdateEmailStatus")]
 		public bool UpdateEmailStatus(int id, short emailStatus, short retryAttampts)
@@ -63,6 +73,11 @@ namespace M4PL.API.Controllers
 			return _emailCommands.UpdateEmailStatus(id, emailStatus, retryAttampts);
 		}
 
+		/// <summary>
+		/// Sends xCBL IT Email Notification for scenario(Event Id) supplied e.g. 5 for xCBL Invalid Requests 
+		/// </summary>
+		/// <param name="scenarioTypeId">Event Id</param>
+		/// <returns></returns>
 		[HttpGet]
 		[Route("xCBLEmailNotification")]
 		public bool xCBLEmailNotification(int scenarioTypeId)
@@ -70,6 +85,12 @@ namespace M4PL.API.Controllers
 			_emailCommands.ActiveUser = Models.ApiContext.ActiveUser;
 			return _emailCommands.xCBLEmailNotification(scenarioTypeId);
 		}
+
+		/// <summary>
+		/// Sends EDI IT Email Notification for scenario(Event Id) supplied e.g. 9 for EDI- No EDI Received (Daily)
+		/// </summary>
+		/// <param name="scenarioTypeId">Event Id</param>
+		/// <returns></returns>
 		[HttpGet]
 		[Route("EDIEmailNotification")]
 		public bool EDIEmailNotification(int scenarioTypeId)
