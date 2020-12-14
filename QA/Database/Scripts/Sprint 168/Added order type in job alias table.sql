1ab7f313383461f6884987c0ba57359906207d2a
kamal.adhikary <kamal.adhikary@DOLAP400>
2020-12-14 13:47:26 +0530

    #396,398

---
+++ QA/Database/Scripts/Sprint 168/Added order type in job alias table.sql
@@ -0,0 +1,5 @@
+
+UPDATE SYSTM000ColumnsAlias SET ColIsVisible=1,ColAliasName='Order Type',ColIsDefault=1,
+ColCaption='Order Type',ColGridAliasName='Order Type',ISGRIDCOLUMN=1
+WHERE  ColTableName='job' and ColColumnName = 'JobType'
+
