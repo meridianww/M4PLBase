04bae2045904619da5f8eb54b20c01ba6262706c
kamal.adhikary <kamal.adhikary@DOLAP400>
2021-01-12 17:29:21 +0530

    Order details model changes

--- QA/M4PL/M4PL.Entities/JobService/OrderDetails.cs
+++ QA/M4PL/M4PL.Entities/JobService/OrderDetails.cs
@@ -76,6 +76,197 @@ namespace M4PL.Entities.JobService
         /// </summary>
         public bool IsJobPermission { get; set; }
         /// <summary>
+		/// Gets or sets the job delivery site poc for job delivery.
+		/// </summary>
+		/// <value>
+		/// The JobDeliverySitePOC.
+		/// </value>
+		public string JobDeliverySitePOC { get; set; }
+
+        /// <summary>
+        /// Gets or sets the .
+        /// </summary>
+        /// <value>
+        /// The JobDeliverySitePOCPhone for job delivery.
+        /// </value>
+        public string JobDeliverySitePOCPhone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the job delivery poc email for job delivery.
+        /// </summary>
+        /// <value>
+        /// The JobDeliverySitePOCEmail.
+        /// </value>
+        public string JobDeliverySitePOCEmail { get; set; }
+
+        /// <summary>
+        /// Gets or sets the .
+        /// </summary>
+        /// <value>
+        /// The JobDeliverySiteName.
+        /// </value>
+        public string JobDeliverySiteName { get; set; }
+
+        /// <summary>
+        /// Gets or sets  job delivery the street address for job delivery.
+        /// </summary>
+        /// <value>
+        /// The JobDeliveryStreetAddress.
+        /// </value>
+        public string JobDeliveryStreetAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier street address2 for job delivery.
+        /// </summary>
+        /// <value>
+        /// The JobDeliveryStreetAddress2.
+        /// </value>
+        public string JobDeliveryStreetAddress2 { get; set; }
+
+
+        /// <summary>
+        /// Gets or sets the identifier street address3 for job delivery.
+        /// </summary>
+        /// <value>
+        /// The JobDeliveryStreetAddress3.
+        /// </value>
+        public string JobDeliveryStreetAddress3 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the identifier street address4 for job delivery.
+        /// </summary>
+        /// <value>
+        /// The JobDeliveryStreetAddress4.
+        /// </value>
+        public string JobDeliveryStreetAddress4 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the delivery city for job delivery.
+        /// </summary>
+        /// <value>
+        /// The JobDeliveryCity.
+        /// </value>
+        public string JobDeliveryCity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state province fo job delivery.
+        /// </summary>
+        /// <value>
+        /// The JobDeliveryStateProvince.
+        /// </value>
+        public string JobDeliveryState { get; set; }
+
+        /// <summary>
+        /// Gets or sets the postal code for job delivery.
+        /// </summary>
+        /// <value>
+        /// The JobDeliveryPostalCode.
+        /// </value>
+        public string JobDeliveryPostalCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the delivery country.
+        /// </summary>
+        /// <value>
+        /// The JobDeliveryCountry.
+        /// </value>
+        public string JobDeliveryCountry { get; set; }
+
+        /// <summary>
+		/// Gets or sets the origin site Poc.
+		/// </summary>
+		/// <value>
+		/// The JobOriginSitePOC.
+		/// </value>
+		public string JobOriginSitePOC { get; set; }
+
+        /// <summary>
+        /// Gets or sets the origin site poc phone.
+        /// </summary>
+        /// <value>
+        /// The JobOriginSitePOCPhone.
+        /// </value>
+        public string JobOriginSitePOCPhone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The JobOriginSitePOCEmail.
+        /// </value>
+        public string JobOriginSitePOCEmail { get; set; }
+
+        /// <summary>
+        /// Gets or sets the site name.
+        /// </summary>
+        /// <value>
+        /// The JobOriginSiteName.
+        /// </value>
+        public string JobOriginSiteName { get; set; }
+
+        /// <summary>
+        /// Gets or sets the street address.
+        /// </summary>
+        /// <value>
+        /// The JobOriginStreetAddress.
+        /// </value>
+        public string JobOriginStreetAddress { get; set; }
+
+        /// <summary>
+        /// Gets or sets the street address2.
+        /// </summary>
+        /// <value>
+        /// The JobOriginStreetAddress2.
+        /// </value>
+        public string JobOriginStreetAddress2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the street address3.
+        /// </summary>
+        /// <value>
+        /// The JobOriginStreetAddress3.
+        /// </value>
+        public string JobOriginStreetAddress3 { get; set; }
+        /// <summary>
+        /// Gets or sets the street address4.
+        /// </summary>
+        /// <value>
+        /// The JobOriginStreetAddress4.
+        /// </value>
+        public string JobOriginStreetAddress4 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The JobOriginCity.
+        /// </value>
+        public string JobOriginCity { get; set; }
+
+        /// <summary>
+        /// Gets or sets the state province.
+        /// </summary>
+        /// <value>
+        /// The JobOriginStateProvince.
+        /// </value>
+        public string JobOriginState { get; set; }
+
+        /// <summary>
+        /// Gets or sets the postalcode.
+        /// </summary>
+        /// <value>
+        /// The JobOriginPostalCode.
+        /// </value>
+        public string JobOriginPostalCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the country.
+        /// </summary>
+        /// <value>
+        /// The JobOriginCountry.
+        /// </value>
+        public string JobOriginCountry { get; set; }
+        /// <summary>
         /// Gets or Sets List of Job Gateways
         /// </summary>
         public List<OrderGatewayDetails> OrderGatewayDetails { get; set; }
