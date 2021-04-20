e0550996bd60ef9248525c7fee9859e74c932861
smbabaz <80287252+smbabaz@users.noreply.github.com>
2021-04-20 17:22:44 -0400

    Fixing build issues

--- QA/M4PL/M4PL.EF/DbEntities.Designer.cs
+++ QA/M4PL/M4PL.EF/DbEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\DreamOrbit\M4PL\M4PL_Base\QA\M4PL\M4PL.EF\DbEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\smbab\source\repos\meridianww\M4PLBase\QA\M4PL\M4PL.EF\DbEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
